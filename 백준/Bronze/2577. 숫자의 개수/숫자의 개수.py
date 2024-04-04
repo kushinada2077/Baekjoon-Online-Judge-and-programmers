30fa5741b96d50437524005049b4b3c44d102fde
kushinada <111179843+movingsummer@users.noreply.github.com>
2024-04-04 19:06:38 +0900

    [Bronze II] Title: 숫자의 개수, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2577. 숫자의 개수/숫자의 개수.py
@@ -0,0 +1,13 @@
+a = int(input())
+b = int(input())
+c = int(input())
+product = str(a * b * c)
+ans = [0] * 10
+
+for num in product:
+  ans[int(num)] += 1
+
+# for n in ans:
+#   print(n)
+
+print("\n".join(map(str, ans)))
