e770e9fe4d5e3a6d4bac5ae55fdeb379e20d9899
kushinada <111179843+kushinada2077@users.noreply.github.com>
2024-05-23 20:15:14 +0900

    [Silver IV] Title: 카드2, Time: 4 ms, Memory: 4156 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.cc
@@ -0,0 +1,24 @@
+#define PAIR pair<long long, int>
+#define ll long long
+#include <algorithm>
+#include <iostream>
+#include <queue>
+#include <vector>
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  queue<int> q;
+  int n;
+  cin >> n;
+  for (int i = 1; i <= n; i++) q.push(i);
+
+  while (q.size() > 1) {
+    q.pop();
+    q.push(q.front());
+    q.pop();
+  }
+
+  cout << q.front();
+}
