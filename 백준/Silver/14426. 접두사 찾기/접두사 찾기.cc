e96214fa3a2276be8c078f56629046d31d4c19fd
kushinada <111179843+kushinada2077@users.noreply.github.com>
2024-05-31 11:21:06 +0900

    [Silver I] Title: 접두사 찾기, Time: 76 ms, Memory: 509836 KB -BaekjoonHub

--- 백준/Silver/14426. 접두사 찾기/접두사 찾기.cc
+++ 백준/Silver/14426. 접두사 찾기/접두사 찾기.cc
@@ -11,18 +11,19 @@ int unused = 2;
 const int MX = 10000 * 500 + 5;
 int nxt[MX][26];
 
+int c2i(char c) { return c - 'a'; }
 void insert(string& s) {
   int cur = ROOT;
   for (auto c : s) {
-    if (!nxt[cur][c]) nxt[cur][c] = unused++;
-    cur = nxt[cur][c];
+    if (!nxt[cur][c2i(c)]) nxt[cur][c2i(c)] = unused++;
+    cur = nxt[cur][c2i(c)];
   }
 }
 bool find(string& s) {
   int cur = ROOT;
   for (auto c : s) {
-    if (!nxt[cur][c]) return false;
-    cur = nxt[cur][c];
+    if (!nxt[cur][c2i(c)]) return false;
+    cur = nxt[cur][c2i(c)];
   }
   return true;
 }
