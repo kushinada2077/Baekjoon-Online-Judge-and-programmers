0bf6fdec0f68c739ffb73380ff070c48110f721d
kushinada <111179843+kushinada2077@users.noreply.github.com>
2024-06-06 10:43:27 +0900

    [Silver III] Title: 01타일, Time: 44 ms, Memory: 40960 KB -BaekjoonHub

---
+++ 백준/Silver/1904. 01타일/01타일.cc
@@ -0,0 +1,31 @@
+#define ll long long
+#include <algorithm>
+#include <iostream>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+using namespace std;
+
+const int M = 15746;
+ll d[1000005][2];
+
+ll f(int n, int k) {
+  if (n == 1) return k == 0 ? 0 : 1;
+  if (n == 2) return 1;
+  if (d[n][k] != -1) return d[n][k];
+
+  int result = 0;
+  if (k == 0) result = result + ((f(n - 2, 0) + f(n - 2, 1)) % M);
+  if (k == 1) result = (result + ((f(n - 1, 0) + f(n - 1, 1)) % M)) % M;
+  d[n][k] = result;
+  return result;
+}
+
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  for (int i = 0; i < 1000005; ++i) fill(d[i], d[i] + 2, -1);
+  int n;
+  cin >> n;
+  cout << (f(n, 0) + f(n, 1)) % M;
+}
