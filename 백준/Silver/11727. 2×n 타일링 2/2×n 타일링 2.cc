215e4b2dd16afb144898c312e959f8f58f51db51
kushinada <111179843+kushinada2077@users.noreply.github.com>
2024-06-06 11:19:04 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 0 ms, Memory: 2044 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.cc
@@ -0,0 +1,32 @@
+#define ll long long
+#include <algorithm>
+#include <iostream>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+using namespace std;
+
+const int M = 10007;
+ll d[1005][3];
+
+ll f(int n, int k) {
+  if (n == 1) return k == 1 ? 1 : 0;
+  if (n == 2) return 1;
+  if (d[n][k] != -1) return d[n][k];
+
+  ll result;
+  if (k == 0) result = (f(n - 2, 0) + f(n - 2, 1) + f(n - 2, 2)) % M;
+  if (k == 1) result = (f(n - 1, 0) + f(n - 1, 1) + f(n - 1, 2)) % M;
+  if (k == 2) result = (f(n - 2, 0) + f(n - 2, 1) + f(n - 2, 2)) % M;
+  d[n][k] = result;
+  return result;
+}
+
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  for (int i = 0; i < 1005; ++i) fill(d[i], d[i] + 3, -1);
+  int n;
+  cin >> n;
+  cout << ((f(n, 0) + f(n, 1)) + f(n, 2)) % M;
+}
