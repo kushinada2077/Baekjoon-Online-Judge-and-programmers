37503e3861214285f7ccc3c6d28ce86cf8718bcd
kushinada <111179843+kushinada2077@users.noreply.github.com>
2024-06-07 11:55:33 +0900

    [Silver I] Title: 쉬운 계단 수, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.cc
+++ 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.cc
@@ -11,14 +11,14 @@ int d[105][10];
 
 // 처음 숫자가 k면서 길이가 n인 계단수의 개수
 ll f(int n, int k) {
-  if (n == 1) return k ? 1 : 0;
+  if (n == 1) return 1;
   if (d[n][k]) return d[n][k];
 
   ll result;
-  if (k == 0)
-    result = f(n - 1, k + 1) % M;
-  else if (k == 9)
+  if (k == 9)
     result = f(n - 1, k - 1) % M;
+  else if (k == 0)
+    result = f(n - 1, k + 1) % M;
   else
     result = (f(n - 1, k + 1) + f(n - 1, k - 1)) % M;
   d[n][k] = result;
@@ -30,6 +30,6 @@ int main() {
   cin.tie(0);
   cin >> N;
   int sum = 0;
-  for (int i = 0; i < 10; ++i) sum = (sum + f(N, i)) % M;
+  for (int i = 1; i < 10; ++i) sum = (sum + f(N, i)) % M;
   cout << sum;
 }
