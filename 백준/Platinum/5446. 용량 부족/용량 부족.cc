be7973055bd31eb516f349fc1ecffece1da65660
kushinada <111179843+kushinada2077@users.noreply.github.com>
2024-06-07 15:26:41 +0900

    [Platinum III] Title: 용량 부족, Time: 12 ms, Memory: 3784 KB -BaekjoonHub

--- 백준/Platinum/5446. 용량 부족/용량 부족.cc
+++ 백준/Platinum/5446. 용량 부족/용량 부족.cc
@@ -1,4 +1,3 @@
-#define PAIR pair<long long, int>
 #define ll long long
 #include <algorithm>
 #include <iostream>
@@ -7,77 +6,71 @@
 #include <vector>
 using namespace std;
 
-const int MX = 1000 * 20 + 5;
 const int ROOT = 1;
-const string chars =
-    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789.";
+const int MX = 2000 * 20 + 5;
+unordered_set<int> x;
+vector<pair<char, int>> nxt[MX];
+int t, n, cnt;
 int unused = 2;
-int nxt[MX][63];
-bool chk[MX];
-int cnt;
 string s;
-unordered_map<char, int> c2i;
-unordered_set<int> po;
 
-void setup() {
-  for (int i = 0; i < chars.size(); ++i) c2i[chars[i]] = i;
+int find(char c, int v) {
+  for (auto [d, nv] : nxt[v]) {
+    if (c == d) return nv;
+  }
+  return -1;
 }
 
 void insert(string& s) {
   int v = ROOT;
   for (auto c : s) {
-    if (nxt[v][c2i[c]] == 0) nxt[v][c2i[c]] = unused++;
-    v = nxt[v][c2i[c]];
+    int nv = find(c, v);
+    if (nv == -1) {
+      nv = unused;
+      nxt[v].push_back({c, unused++});
+    }
+    v = nv;
   }
-  chk[v] = true;
 }
 
-void find(string& s) {
+void locate(string& s) {
   int v = ROOT;
   for (auto c : s) {
-    if (nxt[v][c2i[c]] == 0) {
-      po.insert(unused);
-      nxt[v][c2i[c]] = unused++;
+    int nv = find(c, v);
+    if (nv == -1) {
+      x.insert(unused);
+      nxt[v].push_back({c, unused++});
       return;
     }
-    if (po.find(nxt[v][c2i[c]]) != po.end()) return;
-    v = nxt[v][c2i[c]];
+    if (x.find(nv) != x.end()) return;
+    v = nv;
   }
+
   cnt++;
 }
 
 int main() {
   ios::sync_with_stdio(0);
   cin.tie(0);
-  setup();
-  int t, n;
   cin >> t;
-
   while (t--) {
-    cnt = 0;
-    for (int i = 0; i < MX; ++i) fill(nxt[i], nxt[i] + 63, 0);
-    fill(chk, chk + MX, 0);
-    po.clear();
-    unused = 2;
-    vector<string> file;
     cin >> n;
-    while (n--) {
-      cin >> s;
-      file.push_back(s);
-    }
+    unused = 2;
+    cnt = 0;
+    for (int i = 0; i < MX; ++i) nxt[i].clear();
+    x.clear();
+    vector<string> file(n);
+    for (int i = 0; i < n; ++i) cin >> file[i];
     cin >> n;
     if (n == 0) {
       cout << "1\n";
       continue;
     }
-    while (n--) {
+    for (int i = 0; i < n; ++i) {
       cin >> s;
       insert(s);
     }
-
-    for (auto f : file) {
-      find(f);
-    }
-    cout << po.size() + cnt << "\n";
+    for (auto f : file) locate(f);
+    cout << x.size() + cnt << "\n";
   }
 }
