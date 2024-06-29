bd2dd0481c4b2808cb355208683885d1e45951e9
kushinada <111179843+kushinada2077@users.noreply.github.com>
2024-06-29 22:40:09 +0900

    [Bronze V] Title: A×B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.cc
@@ -0,0 +1,13 @@
+#define ll long long
+#include <iostream>
+#include <set>
+#include <vector>
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  int a, b;
+  cin >> a >> b;
+  cout << a * b;
+}
