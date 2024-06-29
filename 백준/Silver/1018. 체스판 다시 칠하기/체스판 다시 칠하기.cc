a2eebdd5f8cea049f3932aec836857a53da5c0b3
kushinada <111179843+kushinada2077@users.noreply.github.com>
2024-06-29 23:33:29 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.cc
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.cc
@@ -7,15 +7,6 @@ using namespace std;
 int n, m, ans = 0x3f3f3f3f;
 string board[50];
 bool f(char c) { return c == 'W'; }
-int count(int r, int c) {
-  int cnt = 0;
-  for (int i = r; i < r + 8; ++i) {
-    for (int j = c; j < c + 8; ++j) {
-      if (f(board[i][j]) != ((i + j) & 1)) cnt++;
-    }
-  }
-  return cnt > 32 ? 64 - cnt : cnt;
-}
 int main() {
   ios::sync_with_stdio(0);
   cin.tie(0);
@@ -23,7 +14,13 @@ int main() {
   for (int i = 0; i < n; ++i) cin >> board[i];
   for (int i = 0; i < n - 7; ++i) {
     for (int j = 0; j < m - 7; ++j) {
-      ans = min(ans, count(i, j));
+      int cnt = 0;
+      for (int x = i; x < i + 8; ++x) {
+        for (int y = j; y < j + 8; ++y) {
+          cnt += f(board[x][y]) == ((x + y) & 1);
+        }
+      }
+      ans = min(min(ans, cnt), 64 - cnt);
     }
   }
   cout << ans;
