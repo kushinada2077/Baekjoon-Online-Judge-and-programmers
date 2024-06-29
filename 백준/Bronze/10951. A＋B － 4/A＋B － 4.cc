01c3d0432174851ba1b00346705fb47ae37f6257
kushinada <111179843+kushinada2077@users.noreply.github.com>
2024-06-29 22:34:44 +0900

    [Bronze V] Title: A+B - 4, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.cc
@@ -0,0 +1,14 @@
+#define ll long long
+#include <iostream>
+#include <set>
+#include <vector>
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  int a, b;
+  while (cin >> a >> b) {
+    cout << a + b << "\n";
+  }
+}
