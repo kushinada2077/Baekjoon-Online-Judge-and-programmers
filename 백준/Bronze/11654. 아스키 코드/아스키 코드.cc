865d16822783d1bcfb1ddc82678d1f382405514d
kushinada <111179843+kushinada2077@users.noreply.github.com>
2024-06-29 22:41:04 +0900

    [Bronze V] Title: 아스키 코드, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.cc
@@ -0,0 +1,13 @@
+#define ll long long
+#include <iostream>
+#include <set>
+#include <vector>
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  char a;
+  cin >> a;
+  cout << (int)a;
+}
