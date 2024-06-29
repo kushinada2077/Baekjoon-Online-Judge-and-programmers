089b1736cb48bd05fa3abea8dcbe5022901a9ac3
kushinada <111179843+kushinada2077@users.noreply.github.com>
2024-06-29 22:49:53 +0900

    [Bronze V] Title: 문자와 문자열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.cc
@@ -0,0 +1,14 @@
+#define ll long long
+#include <iostream>
+#include <set>
+#include <vector>
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  string s;
+  int i;
+  cin >> s >> i;
+  cout << s[i - 1];
+}
