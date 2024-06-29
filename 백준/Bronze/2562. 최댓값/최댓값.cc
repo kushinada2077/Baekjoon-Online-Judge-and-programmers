ff5097e85e005e60aeccafcc74f624f0a4207c5f
kushinada <111179843+kushinada2077@users.noreply.github.com>
2024-06-29 19:24:03 +0900

    [Bronze III] Title: 최댓값, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2562. 최댓값/최댓값.cc
+++ 백준/Bronze/2562. 최댓값/최댓값.cc
@@ -7,9 +7,9 @@ void merge(vector<int>& a, int st, int en) {
   vector<int> tmp;
   int mid = (st + en) / 2;
   int l = st;
-  int r = mid + 1;
+  int r = mid;
 
-  while (l <= mid && r <= en) {
+  while (l < mid && r < en) {
     if (a[l] > a[r])
       tmp.push_back(a[l++]);
     else
@@ -18,13 +18,13 @@ void merge(vector<int>& a, int st, int en) {
 
   while (l <= mid) tmp.push_back(a[l++]);
   while (r <= en) tmp.push_back(a[r++]);
-  for (int i = st; i <= en; ++i) a[i] = tmp[i - st];
+  for (int i = st; i < en; ++i) a[i] = tmp[i - st];
 }
 void partition(vector<int>& a, int st, int en) {
-  if (st == en) return;
+  if (st + 1 == en) return;
   int mid = (st + en) / 2;
   partition(a, st, mid);
-  partition(a, mid + 1, en);
+  partition(a, mid, en);
   merge(a, st, en);
 }
 int main() {
@@ -36,6 +36,6 @@ int main() {
     cin >> a[i];
     n2i[a[i]] = i;
   }
-  partition(a, 0, a.size() - 1);
+  partition(a, 0, a.size());
   cout << a[0] << "\n" << n2i[a[0]] + 1;
 }
