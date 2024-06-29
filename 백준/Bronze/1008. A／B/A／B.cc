0930e561a4eb5ec7fbab7cb75ded859960b7cd3f
kushinada <111179843+kushinada2077@users.noreply.github.com>
2024-06-29 18:19:30 +0900

    [Bronze V] Title: A/B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.cc
@@ -0,0 +1,13 @@
+#define ll long long
+#include <iostream>
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  cout << fixed;
+  cout.precision(10);
+  double a, b;
+  cin >> a >> b;
+  cout << a / b;
+}
