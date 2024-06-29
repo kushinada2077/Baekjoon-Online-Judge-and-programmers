f31ac7bb2707962e3efed6b9326c72e2086c191c
kushinada <111179843+kushinada2077@users.noreply.github.com>
2024-06-29 22:48:48 +0900

    [Bronze V] Title: 새싹, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.cc
@@ -0,0 +1,12 @@
+#define ll long long
+#include <iostream>
+#include <set>
+#include <vector>
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  cout << "         ,r'\"7\nr`-_   ,'  ,/\n \\. \". L_r'\n   `~\\/\n      |\n  "
+          "    |";
+}
