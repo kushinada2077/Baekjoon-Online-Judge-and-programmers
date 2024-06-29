9407d724a2cc8a53c5c78cdd4a27cd3d09dde4cf
kushinada <111179843+kushinada2077@users.noreply.github.com>
2024-06-29 19:33:58 +0900

    [Bronze V] Title: N 찍기, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.cc
@@ -0,0 +1,12 @@
+#define ll long long
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int n;
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  cin >> n;
+  for (int i = 0; i < n; ++i) cout << i + 1 << "\n";
+}
