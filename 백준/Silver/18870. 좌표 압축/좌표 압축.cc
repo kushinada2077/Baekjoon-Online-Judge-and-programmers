bbf6db7992cc5a88bdf1d31ebd046a4d2dec72da
kushinada <111179843+kushinada2077@users.noreply.github.com>
2024-06-30 13:27:32 +0900

    [Silver II] Title: 좌표 압축, Time: 480 ms, Memory: 9840 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.cc
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.cc
@@ -1,13 +1,11 @@
 #include <algorithm>
 #include <iostream>
-#include <unordered_map>
 #include <vector>
 #define ll long long
 #define fastio cin.tie(0)->sync_with_stdio(0);
 using namespace std;
 
-unordered_map<int, int> d;
-int upper_bound(vector<int>& a, int x) {
+int up(vector<int>& a, int x) {
   int lo = -1, hi = a.size();
   while (lo + 1 < hi) {
     int mid = (lo + hi) / 2;
@@ -16,26 +14,17 @@ int upper_bound(vector<int>& a, int x) {
   }
   return hi;
 }
-int compress(vector<int>& a, int x) {
-  if (d.find(x) != d.end()) return d[x];
-  d[x] = upper_bound(a, x);
-  return d[x];
-}
 int main() {
   fastio;
   int n;
   cin >> n;
   vector<int> a(n);
-  vector<int> erase_dup;
-  vector<int> tmp(n);
+  vector<int> uni(n);
   for (int i = 0; i < n; ++i) {
-    cin >> tmp[i];
-    a[i] = tmp[i];
+    cin >> uni[i];
+    a[i] = uni[i];
   }
-  sort(tmp.begin(), tmp.end());
-  for (int i = 0; i < n; ++i) {
-    if (i > 0 && tmp[i] == tmp[i - 1]) continue;
-    erase_dup.push_back(tmp[i]);
-  }
-  for (auto v : a) cout << compress(erase_dup, v) << " ";
+  sort(uni.begin(), uni.end());
+  uni.erase(unique(uni.begin(), uni.end()), uni.end());
+  for (auto v : a) cout << up(uni, v) << " ";
 }
