9e921adb6cfb0d2ef7b3901510c7580e4d3b05d6
kushinada <111179843+kushinada2077@users.noreply.github.com>
2024-06-30 14:07:02 +0900

    [Gold IV] Title: 세 수의 합, Time: 64 ms, Memory: 5228 KB -BaekjoonHub

--- 백준/Gold/2295. 세 수의 합/세 수의 합.cc
+++ 백준/Gold/2295. 세 수의 합/세 수의 합.cc
@@ -5,36 +5,31 @@
 #define fastio cin.tie(0)->sync_with_stdio(0);
 using namespace std;
 
-int bs(vector<pair<int, int>>& a, int x) {
+int bs(vector<int>& a, int x) {
   int lo = -1, hi = a.size();
   while (lo + 1 < hi) {
     int mid = (lo + hi) / 2;
-    if (a[mid].first >= x) hi = mid;
+    if (a[mid] >= x) hi = mid;
     else lo = mid;
   }
-  return a[hi].first == x ? a[hi].second : -1;
+  return a[hi] == x ? a[hi] : -1;
 }
 int main() {
   fastio;
   int n, ans = -1;
   cin >> n;
   vector<int> a(n);
-  vector<pair<int, int>> diff;
-  for (int i = 0; i < n; ++i) cin >> a[i];
+  vector<int> two_sum;
   sort(a.begin(), a.end());
-  for (int i = 0; i < n; i++) {
-    for (int j = i + 1; j < n; ++j) {
-      if (a[j] - a[i] > 0) diff.push_back({a[j] - a[i], a[i]});
-    }
-  }
-  sort(diff.begin(), diff.end());
-  diff.erase(unique(diff.begin(), diff.end()), diff.end());
+  for (int i = 0; i < n; ++i) cin >> a[i];
+  for (int i = 0; i < n; ++i)
+    for (int j = i; j < n; ++j) two_sum.push_back(a[i] + a[j]);
+  sort(two_sum.begin(), two_sum.end());
+  two_sum.erase(unique(two_sum.begin(), two_sum.end()), two_sum.end());
   for (int i = 0; i < n; ++i) {
     for (int j = i; j < n; ++j) {
-      int apb = a[i] + a[j];
-      if (apb < 0) continue;
-      int c = bs(diff, apb);
-      if (c != -1) ans = max(ans, apb + c);
+      int apb = bs(two_sum, a[j] - a[i]);
+      if (apb != -1) ans = max(ans, apb + a[i]);
     }
   }
   cout << ans;
