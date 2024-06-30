3963b5232e860d54c2e5ff1f3c11ca00d6a24014
kushinada <111179843+kushinada2077@users.noreply.github.com>
2024-06-30 14:12:40 +0900

    [Gold IV] Title: 세 수의 합, Time: 28 ms, Memory: 5228 KB -BaekjoonHub

--- 백준/Gold/2295. 세 수의 합/세 수의 합.cc
+++ 백준/Gold/2295. 세 수의 합/세 수의 합.cc
@@ -5,31 +5,32 @@
 #define fastio cin.tie(0)->sync_with_stdio(0);
 using namespace std;
 
-int bs(vector<int>& a, int x) {
+bool bs(vector<int>& a, int x) {
   int lo = -1, hi = a.size();
   while (lo + 1 < hi) {
     int mid = (lo + hi) / 2;
     if (a[mid] >= x) hi = mid;
     else lo = mid;
   }
-  return a[hi] == x ? a[hi] : -1;
+  return a[hi] == x;
 }
 int main() {
   fastio;
-  int n, ans = -1;
+  int n;
   cin >> n;
   vector<int> a(n);
   vector<int> two_sum;
-  sort(a.begin(), a.end());
   for (int i = 0; i < n; ++i) cin >> a[i];
+  sort(a.begin(), a.end());
   for (int i = 0; i < n; ++i)
     for (int j = i; j < n; ++j) two_sum.push_back(a[i] + a[j]);
   sort(two_sum.begin(), two_sum.end());
-  for (int i = 0; i < n; ++i) {
-    for (int j = i; j < n; ++j) {
-      int apb = bs(two_sum, a[j] - a[i]);
-      if (apb != -1) ans = max(ans, apb + a[i]);
+  for (int i = n - 1; i > 0; --i) {
+    for (int j = 0; j < i; ++j) {
+      if (bs(two_sum, a[i] - a[j])) {
+        cout << a[i];
+        return 0;
+      }
     }
   }
-  cout << ans;
 }
