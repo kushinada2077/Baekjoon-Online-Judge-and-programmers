bd6f5e6c2332789ef2e9c13738446311eb932f76
kushinada <111179843+kushinada2077@users.noreply.github.com>
2024-06-30 13:03:07 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 288 ms, Memory: 80144 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.cc
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.cc
@@ -6,37 +6,19 @@
 #define fastio cin.tie(0)->sync_with_stdio(0);
 using namespace std;
 
-void merge(vector<int> &a, int st, int en) {
-  vector<int> tmp(en - st);
-  int n = a.size();
-  int mid = (st + en) / 2;
-  int l = st;
-  int r = mid;
-  for (int i = st; i < en; ++i) {
-    if (l == mid) tmp[i - st] = a[r++];
-    else if (r == en) tmp[i - st] = a[l++];
-    else if (a[l] <= a[r]) tmp[i - st] = a[l++];
-    else tmp[i - st] = a[r++];
-  }
-  for (int i = st; i < en; ++i) a[i] = tmp[i - st];
-}
-void merge_sort(vector<int> &a, int st, int en) {
-  if (st + 1 == en) return;
-  int mid = (st + en) / 2;
-  merge_sort(a, st, mid);
-  merge_sort(a, mid, en);
-  merge(a, st, en);
-}
+int cnt[20000010];
 int main() {
   fastio;
+  const int OFF = 10000000;
   int n, m, x;
   cin >> n;
-  vector<int> a(n);
-  for (int i = 0; i < n; ++i) cin >> a[i];
-  merge_sort(a, 0, n);
+  for (int i = 0; i < n; ++i) {
+    cin >> x;
+    cnt[x + OFF]++;
+  }
   cin >> m;
   while (m--) {
     cin >> x;
-    cout << upper_bound(a.begin(), a.end(), x) - lower_bound(a.begin(), a.end(), x) << " ";
+    cout << cnt[x + OFF] << " ";
   }
 }
