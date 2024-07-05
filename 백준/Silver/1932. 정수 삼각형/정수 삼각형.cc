08142b23e4fd9b909450fc3d566112691ae24c59
kushinada <111179843+kushinada2077@users.noreply.github.com>
2024-07-05 16:32:35 +0900

    [Silver I] Title: 정수 삼각형, Time: 12 ms, Memory: 4076 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.cc
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.cc
@@ -16,23 +16,14 @@ using namespace std;
 
 int d[505][505];
 // f(n, k) => 좌표 (n(row), k(col))까지 도달한 합
-int f(vector<vector<int>>& tri, int n, int k) {
-  if (n == 1) return tri[1][1];
-  if (d[n][k] != -1) return d[n][k];
-
-  int ret = max(f(tri, n - 1, k - 1), f(tri, n - 1, k)) + tri[n][k];
-  d[n][k] = ret;
-  return ret;
-}
 int main() {
   fastio;
-  int n, x, ans = 0;
+  int n;
   cin >> n;
-  for_in(i, 0, n + 1) fill(d[i], d[i] + n + 1, -1);
   vector<vector<int>> tri(n + 1, vector<int>(n + 1, 0));
-  for_in(i, 1, n + 1) {
-    for_in(j, 0, i) { cin >> tri[i][j + 1]; }
+  for_in(i, n) {
+    for_in(j, i + 1) { cin >> tri[i + 1][j + 1]; }
   }
-  for_in(i, 1, n + 1) ans = max(ans, f(tri, n, i));
-  cout << ans << "\n";
+  for_in(i, 1, n + 1) for_in(j, 1, i + 1) d[i][j] = max(d[i - 1][j - 1], d[i - 1][j]) + tri[i][j];
+  cout << *max_element(d[n] + 1, d[n] + n + 1) << "\n";
 }
