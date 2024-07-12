12d3a78d6929c77ea965c288f1ec3e5a9461cc58
kushinada <111179843+kushinada2077@users.noreply.github.com>
2024-07-13 01:04:02 +0900

    [Silver I] Title: 카드 구매하기, Time: 4 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/11052. 카드 구매하기/카드 구매하기.cc
+++ 백준/Silver/11052. 카드 구매하기/카드 구매하기.cc
@@ -1,31 +1,32 @@
-#define ll long long
 #include <algorithm>
 #include <iostream>
-#include <unordered_map>
-#include <unordered_set>
+#include <set>
 #include <vector>
+#define ll long long
+#define fastio cin.tie(0)->sync_with_stdio(0);
+#define for_in(n) for (int i = 0; i < n; ++i)
+#define si(x) int(x.size())
+#define all(x) (x).begin(), (x).end()
+#define pb(...) push_back(__VA_ARGS__)
+#define X first
+#define Y second
 using namespace std;
 
-int N;
-int d[1001][1001];
-int card[1001];
-const int IMPOSSIBLE = -0x3f3f3f3f;
-// n개를 k번째 팩부터 샀을 때 최댓값
-int f(int n, int k) {
-  if (k == N) return n ? IMPOSSIBLE : 0;
-  if (d[n][k] != -1) return d[n][k];
-
-  int result = f(n, k + 1);
-  if (n >= k + 1) result = max(result, f(n - k - 1, k) + card[k]);
-  d[n][k] = result;
-  return result;
+int d[1005];
+int f(vector<int>& a, int k) {
+  if (k == 0) return 0;
+  if (k < 0) return -0x3f3f3f3f;
+  if (d[k]) return d[k];
+  int ret = 0;
+  for (int i = 0; i < si(a); ++i) ret = max(ret, a[i] + f(a, k - i - 1));
+  d[k] = ret;
+  return ret;
 }
-
 int main() {
-  ios::sync_with_stdio(0);
-  cin.tie(0);
-  cin >> N;
-  for (int i = 0; i < 1001; ++i) fill(d[i], d[i] + 1001, -1);
-  for (int i = 0; i < N; ++i) cin >> card[i];
-  cout << f(N, 0);
+  fastio;
+  int n;
+  cin >> n;
+  vector<int> a(n);
+  for (auto& i : a) cin >> i;
+  cout << f(a, n) << "\n";
 }
