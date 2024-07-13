ed3f3274c66095efd56fb44daa1344c6f04e62f6
kushinada <111179843+kushinada2077@users.noreply.github.com>
2024-07-13 16:38:46 +0900

    [Silver V] Title: 셀프 넘버, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/4673. 셀프 넘버/셀프 넘버.cc
@@ -0,0 +1,29 @@
+#include <algorithm>
+#include <iostream>
+#include <set>
+#include <vector>
+#define ll long long
+#define fastio cin.tie(0)->sync_with_stdio(0);
+#define for_in(n) for (int i = 0; i < n; ++i)
+#define si(x) int(x.size())
+#define all(x) (x).begin(), (x).end()
+#define pb(...) push_back(__VA_ARGS__)
+#define X first
+#define Y second
+using namespace std;
+
+int f(int n) {
+  int ret = n;
+  while (n > 0) {
+    ret += n % 10;
+    n /= 10;
+  }
+  return ret;
+}
+int main() {
+  fastio;
+  vector<bool> chk(10005, false);
+  for (int i = 1; i < 10001; ++i) chk[f(i)] = true;
+  for (int i = 1; i < 10001; ++i)
+    if (!chk[i]) cout << i << "\n";
+}
