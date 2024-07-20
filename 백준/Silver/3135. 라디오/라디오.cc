5afbf84897d39e5e17e1739292c447cd29ed1014
kushinada <111179843+kushinada2077@users.noreply.github.com>
2024-07-20 17:43:38 +0900

    [Silver V] Title: 라디오, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/3135. 라디오/라디오.cc
+++ 백준/Silver/3135. 라디오/라디오.cc
@@ -12,39 +12,15 @@
 #define Y second
 using namespace std;
 
-int bfs(vector<int>& predf, int a, int b) {
-  queue<int> q;
-  vector<int> dist(1005, -1);
-  if (a + 1 < 1000) {
-    q.push(a + 1);
-    dist[a + 1] = 1;
-  }
-  if (a - 1 > 0) {
-    q.push(a - 1);
-    dist[a - 1] = 1;
-  }
-  for (auto& pre : predf) {
-    q.push(pre);
-    dist[pre] = 1;
-  }
-
-  while (!q.empty()) {
-    auto cur = q.front();
-    if (cur == b) return dist[cur];
-    q.pop();
-    for (int i = -1; i < 2; i += 2) {
-      int nxt = cur + i;
-      if (nxt < 1 || nxt >= 1000 || dist[nxt] != -1) continue;
-      q.push(nxt);
-      dist[nxt] = dist[cur] + 1;
-    }
-  }
-}
 int main() {
   fastio;
   int a, b, n;
   cin >> a >> b >> n;
-  vector<int> predf(n);
-  for (auto& i : predf) cin >> i;
-  cout << bfs(predf, a, b) << "\n";
+  vector<int> predef(n);
+  for (auto& i : predef) cin >> i;
+  int ans = abs(b - a);
+  for (auto& pre : predef) {
+    ans = min(ans, abs(pre - b) + 1);
+  }
+  cout << ans << "\n";
 }
