3c0cf3871737ce78f36d3a4dfdebdf8a651412bd
kushinada <111179843+kushinada2077@users.noreply.github.com>
2024-07-25 21:20:17 +0900

    [Silver IV] Title: 카드2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.cc
+++ 백준/Silver/2164. 카드2/카드2.cc
@@ -1,24 +1,26 @@
-#define PAIR pair<long long, int>
-#define ll long long
 #include <algorithm>
+#include <deque>
 #include <iostream>
+#include <numeric>
 #include <queue>
+#include <stack>
+#include <tuple>
 #include <vector>
+#define ll long long
+#define fastio cin.tie(0)->sync_with_stdio(0);
+#define for_in(n) for (int i = 0; i < n; ++i)
+#define si(x) int(x.size())
+#define all(x) (x).begin(), (x).end()
+#define pb(...) push_back(__VA_ARGS__)
+#define X first
+#define Y second
 using namespace std;
 
 int main() {
-  ios::sync_with_stdio(0);
-  cin.tie(0);
-  queue<int> q;
-  int n;
+  fastio;
+  int n, x = 1;
   cin >> n;
-  for (int i = 1; i <= n; i++) q.push(i);
-
-  while (q.size() > 1) {
-    q.pop();
-    q.push(q.front());
-    q.pop();
-  }
-
-  cout << q.front();
+  while (n >= (x << 1)) x <<= 1;
+  if (n == x) cout << n;
+  else cout << 2 * (n - x) << "\n";
 }
