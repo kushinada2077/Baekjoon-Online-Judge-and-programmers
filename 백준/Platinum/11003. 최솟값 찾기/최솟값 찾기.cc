0be85b220cc5d5ac5b5107381a4960683d2ab8c0
kushinada <111179843+kushinada2077@users.noreply.github.com>
2024-07-29 00:30:23 +0900

    [Platinum V] Title: 최솟값 찾기, Time: 972 ms, Memory: 125592 KB -BaekjoonHub

--- 백준/Platinum/11003. 최솟값 찾기/최솟값 찾기.cc
+++ 백준/Platinum/11003. 최솟값 찾기/최솟값 찾기.cc
@@ -1,6 +1,10 @@
 #include <algorithm>
+#include <cmath>
 #include <deque>
 #include <iostream>
+#include <map>
+#include <queue>
+#include <tuple>
 #include <vector>
 #define fastio cin.tie(0)->sync_with_stdio(0);
 #define for_in(n) for (int i = 0; i < n; ++i)
@@ -12,12 +16,24 @@
 using ll = long long;
 using namespace std;
 
+vector<int> split(const string& s, const string& sep) {
+  int pos = 0;
+  vector<int> ret;
+  while (pos < si(s)) {
+    int nxt_pos = s.find(sep, pos);
+    if (nxt_pos == -1) nxt_pos = si(s);
+    if (nxt_pos - pos > 0) ret.pb(stoi(s.substr(pos, nxt_pos - pos)));
+    pos = nxt_pos + si(sep);
+  }
+  return ret;
+}
 int main() {
   fastio;
   int n, L;
   deque<int> q;
   cin >> n >> L;
   vector<int> a(n);
+  string ans = "";
   for (auto& i : a) cin >> i;
   int l = 0;
   for (int r = 0; r < n; ++r) {
@@ -27,6 +43,7 @@ int main() {
       if (q.front() == a[l]) q.pop_front();
       l++;
     }
-    cout << q.front() << " ";
+    ans += to_string(q.front()) + " ";
   }
+  cout << ans << "\n";
 }
