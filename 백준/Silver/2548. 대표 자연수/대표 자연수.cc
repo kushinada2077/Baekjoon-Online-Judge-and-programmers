3a09102d3a9fbb13a350e67c9d32196b7f08684b
kushinada <111179843+kushinada2077@users.noreply.github.com>
2024-07-29 13:09:32 +0900

    [Silver III] Title: 대표 자연수, Time: 0 ms, Memory: 2220 KB -BaekjoonHub

--- 백준/Silver/2548. 대표 자연수/대표 자연수.cc
+++ 백준/Silver/2548. 대표 자연수/대표 자연수.cc
@@ -20,19 +20,25 @@ int main() {
   fastio;
   int n;
   cin >> n;
-  vector<int> a(n), sum(10001, 0);
-  for (auto& i : a) cin >> i;
-  sort(all(a));
-  int ans, mx = 0x3f3f3f3f;
-  for (int i = a[n - 1]; i >= a[0]; --i) {
-    int tot = 0;
-    for (int j = 0; j < n; ++j) {
-      tot += abs(i - a[j]);
-    }
-    if (tot <= mx) {
-      ans = i;
-      mx = tot;
+  vector<int> a(n), cnt(10005, 0), sum(10005, 0);
+  for (auto& i : a) {
+    cin >> i;
+    cnt[i]++;
+  }
+  int tot = 0, num = 0;
+  for (int i = 1; i < 10001; ++i) {
+    tot += i * cnt[i];
+    sum[i] = tot;
+    num += cnt[i];
+    cnt[i] = num;
+  }
+  int comp = 0x3f3f3f3f, idx = 0;
+  for (int i = 1; i < 10001; ++i) {
+    int diff = (i * cnt[i] - sum[i]) + (sum[10000] - sum[i]) - (i * (cnt[10000] - cnt[i]));
+    if (diff < comp) {
+      comp = diff;
+      idx = i;
     }
   }
-  cout << ans << "\n";
+  cout << idx << "\n";
 }
