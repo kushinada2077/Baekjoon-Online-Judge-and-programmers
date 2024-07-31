c9bebce87a4c60942cbdbb4c9ef3d91ed906e26f
kushinada <111179843+kushinada2077@users.noreply.github.com>
2024-07-31 19:33:08 +0900

    [Silver IV] Title: 수 찾기, Time: 52 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.cc
+++ 백준/Silver/1920. 수 찾기/수 찾기.cc
@@ -1,57 +1,39 @@
 #include <algorithm>
+#include <climits>
+#include <cmath>
+#include <deque>
 #include <iostream>
+#include <queue>
+#include <tuple>
 #include <unordered_set>
 #include <vector>
-#define ll long long
 #define fastio cin.tie(0)->sync_with_stdio(0);
+#define for_in(n) for (int i = 0; i < n; ++i)
+#define si(x) int(x.size())
+#define all(x) (x).begin(), (x).end()
+#define pb(...) push_back(__VA_ARGS__)
+#define X first
+#define Y second
+using ll = long long;
 using namespace std;
 
-void merge(vector<int>& a, int st, int en) {
-  vector<int> tmp(en - st);
-  int mid = (st + en) / 2;
-  int l = st;
-  int r = mid;
-  for (int i = st; i < en; ++i) {
-    if (l == mid)
-      tmp[i - st] = a[r++];
-    else if (r == en)
-      tmp[i - st] = a[l++];
-    else if (a[l] <= a[r])
-      tmp[i - st] = a[l++];
-    else
-      tmp[i - st] = a[r++];
-  }
-  for (int i = st; i < en; ++i) a[i] = tmp[i - st];
-}
-void merge_sort(vector<int>& a, int st, int en) {
-  if (st + 1 == en) return;
-  int mid = (st + en) / 2;
-  merge_sort(a, st, mid);
-  merge_sort(a, mid, en);
-  merge(a, st, en);
-}
-bool binary_search(vector<int>& a, int x) {
-  int n = a.size();
-  int lo = -1, hi = n - 1;
-  while (lo + 1 < hi) {
-    int mid = (lo + hi) / 2;
-    if (a[mid] >= x)
-      hi = mid;
-    else
-      lo = mid;
-  }
-  return a[hi] == x;
-}
 int main() {
   fastio;
   int n, m, x;
   cin >> n;
   vector<int> a(n);
-  for (int i = 0; i < n; ++i) cin >> a[i];
-  merge_sort(a, 0, a.size());
+  for (auto& i : a) cin >> i;
+  sort(all(a));
   cin >> m;
-  while (m--) {
+  for (int i = 0; i < m; ++i) {
     cin >> x;
-    cout << binary_search(a, x) << "\n";
+    int lo = 0, hi = n;
+    while (lo + 1 < hi) {
+      int mid = (lo + hi) / 2;
+      if (a[mid] > x) hi = mid;
+      else lo = mid;
+    }
+    if (a[lo] == x) cout << "1\n";
+    else cout << "0\n";
   }
 }
