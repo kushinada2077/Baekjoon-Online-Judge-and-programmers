8a374e717c0c0e577f5e23b96328c15f2cd8a0c0
kushinada <111179843+kushinada2077@users.noreply.github.com>
2024-08-05 19:42:28 +0900

    [Bronze II] Title: 부분 문자열, Time: 12 ms, Memory: 8060 KB -BaekjoonHub

--- 백준/Bronze/16916. 부분 문자열/부분 문자열.cc
+++ 백준/Bronze/16916. 부분 문자열/부분 문자열.cc
@@ -1,38 +1,46 @@
-#define PAIR pair<long long, int>
-#define ll long long
 #include <algorithm>
+#include <climits>
+#include <deque>
 #include <iostream>
+#include <numeric>
+#include <queue>
+#include <tuple>
+#include <unordered_map>
 #include <vector>
+#define fastio cin.tie(0)->sync_with_stdio(0);
+#define for_in(n) for (int i = 0; i < n; ++i)
+#define si(x) int(x.size())
+#define all(x) (x).begin(), (x).end()
+#define pb(...) push_back(__VA_ARGS__)
+#define X first
+#define Y second
+using ll = long long;
 using namespace std;
 
-vector<int> failure(string& s) {
-  vector<int> f(s.size());
+// 실패함수 f(x) = [0:x+1] 문자열에서 일치하는 접두사, 접미사의 최대 길이
+
+vector<int> failure(const string& s) {
+  vector<int> f(si(s));
   int j = 0;
-  for (int i = 1; i < s.size(); i++) {
-    while (j > 0 && s[i] != s[j]) j = f[j - 1];
+  for (int i = 1; i < si(s); ++i) {
+    while (j && s[i] != s[j]) j = f[j - 1];
     if (s[i] == s[j]) f[i] = ++j;
   }
   return f;
 }
-
-int main() {
-  ios::sync_with_stdio(0);
-  cin.tie(0);
-  string s, p;
-  getline(cin, s);
-  getline(cin, p);
+bool kmp(const string& s, const string& p) {
   vector<int> f = failure(p);
-  int ans = 0, j = 0;
-
-  for (int i = 0; i < s.size(); i++) {
-    while (j > 0 && s[i] != p[j]) j = f[j - 1];
+  int j = 0;
+  for (int i = 0; i < si(s); ++i) {
+    while (j && s[i] != p[j]) j = f[j - 1];
     if (s[i] == p[j]) j++;
-
-    if (j == p.size()) {
-      ans = 1;
-      break;
-    }
+    if (j == si(p)) return true;
   }
-
-  cout << ans;
+  return false;
+}
+int main() {
+  fastio;
+  string s, p;
+  cin >> s >> p;
+  cout << kmp(s, p) << "\n";
 }
