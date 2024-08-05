c5cd7a23e1dc5c4c9d0a88ef059555bc56cdd334
kushinada <111179843+kushinada2077@users.noreply.github.com>
2024-08-05 20:43:48 +0900

    [Platinum IV] Title: 시저 암호, Time: 508 ms, Memory: 3392 KB -BaekjoonHub

--- 백준/Platinum/1893. 시저 암호/시저 암호.cc
+++ 백준/Platinum/1893. 시저 암호/시저 암호.cc
@@ -17,25 +17,26 @@
 using ll = long long;
 using namespace std;
 
+int l;
 string a, w, s;
-vector<int> f;
-unordered_map<char, int> c2i;
-char shift(char c, int d) { return a[(c2i[c] + d) % si(a)]; }
+vector<int> f, c2i(256);
+char shift(char c, int d) { return a[(c2i[c] + d) % l]; }
 vector<int> failure(const string& s) {
-  vector<int> f(si(s));
+  int l = si(s);
+  vector<int> f(l);
   int j = 0;
-  for (int i = 1; i < si(s); ++i) {
+  for (int i = 1; i < l; ++i) {
     while (j && s[i] != s[j]) j = f[j - 1];
     if (s[i] == s[j]) f[i] = ++j;
   }
   return f;
 }
 bool kmp(const string& s, const string& p, int d) {
-  int cnt = 0, j = 0;
-  for (int i = 0; i < si(s); ++i) {
+  int cnt = 0, j = 0, ls = si(s), lp = si(p);
+  for (int i = 0; i < ls; ++i) {
     while (j && s[i] != shift(p[j], d)) j = f[j - 1];
     if (s[i] == shift(p[j], d)) ++j;
-    if (j == si(p)) {
+    if (j == lp) {
       cnt++;
       j = f[j - 1];
     }
@@ -50,6 +51,7 @@ int main() {
   while (t--) {
     cin >> a >> w >> s;
     c2i.clear();
+    l = si(a);
     for (int i = 0; i < si(a); ++i) c2i[a[i]] = i;
     f = failure(w);
     vector<int> res;
