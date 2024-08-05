b42a53dc17dd67ccf0634d9b48ae1e71801d8222
kushinada <111179843+kushinada2077@users.noreply.github.com>
2024-08-05 20:30:16 +0900

    [Platinum IV] Title: 시저 암호, Time: 1708 ms, Memory: 3396 KB -BaekjoonHub

--- 백준/Platinum/1893. 시저 암호/시저 암호.cc
+++ 백준/Platinum/1893. 시저 암호/시저 암호.cc
@@ -1,64 +1,66 @@
-
-#define PAIR pair<long long, int>
-#define ll long long
 #include <algorithm>
+#include <climits>
+#include <deque>
 #include <iostream>
-#include <map>
+#include <numeric>
+#include <queue>
+#include <tuple>
+#include <unordered_map>
 #include <vector>
+#define fastio cin.tie(0)->sync_with_stdio(0);
+#define for_in(n) for (int i = 0; i < n; ++i)
+#define si(x) int(x.size())
+#define all(x) (x).begin(), (x).end()
+#define pb(...) push_back(__VA_ARGS__)
+#define X first
+#define Y second
+using ll = long long;
 using namespace std;
 
-int n;
+string a, w, s;
+vector<int> f;
+unordered_map<char, int> c2i;
+char shift(char c, int d) { return a[(c2i[c] + d) % si(a)]; }
 vector<int> failure(const string& s) {
-  vector<int> f(s.size());
+  vector<int> f(si(s));
   int j = 0;
-  for (int i = 1; i < s.size(); ++i) {
-    while (j > 0 && s[i] != s[j]) j = f[j - 1];
+  for (int i = 1; i < si(s); ++i) {
+    while (j && s[i] != s[j]) j = f[j - 1];
     if (s[i] == s[j]) f[i] = ++j;
   }
-
   return f;
 }
+bool kmp(const string& s, const string& p, int d) {
+  int cnt = 0, j = 0;
+  for (int i = 0; i < si(s); ++i) {
+    while (j && s[i] != shift(p[j], d)) j = f[j - 1];
+    if (s[i] == shift(p[j], d)) ++j;
+    if (j == si(p)) {
+      cnt++;
+      j = f[j - 1];
+    }
+    if (cnt > 1) return false;
+  }
+  return cnt == 1;
+}
 int main() {
-  ios::sync_with_stdio(0);
-  cin.tie(0);
-
-  cin >> n;
-  while (n--) {
-    string a, w, s;
+  fastio;
+  int t;
+  cin >> t;
+  while (t--) {
     cin >> a >> w >> s;
-    unordered_map<char, int> idx_map;
-    vector<int> ans;
-    vector<int> f = failure(w);
-    int loop = a.size();
-    for (int i = 0; i < a.size(); ++i) {
-      idx_map[a[i]] = i;
-    }
-    for (int shift = 0; shift < loop; ++shift) {
-      int cnt = 0;
-      int j = 0;
-      for (int i = 0; i < s.size(); ++i) {
-        while (j > 0 && s[i] != a[(idx_map[w[j]] + shift) % loop]) j = f[j - 1];
-        if (s[i] == a[(idx_map[w[j]] + shift) % loop]) j++;
-        if (j == w.size()) {
-          if (cnt) {
-            ans.clear();
-            break;
-          }
-          ans.push_back(shift);
-          j = f[j - 1];
-          cnt++;
-        }
-      }
-    }
-
-    if (ans.size() == 0)
-      cout << "no solution\n";
-    else if (ans.size() == 1)
-      cout << "unique: " << ans[0] << "\n";
+    c2i.clear();
+    for (int i = 0; i < si(a); ++i) c2i[a[i]] = i;
+    f = failure(w);
+    vector<int> res;
+    for (int d = 0; d < si(a); ++d)
+      if (kmp(s, w, d)) res.pb(d);
+    if (si(res) == 0) cout << "no solution\n";
+    else if (si(res) == 1) cout << "unique: " << res[0] << "\n";
     else {
-      sort(ans.begin(), ans.end());
-      cout << "ambiguous:";
-      for (auto n : ans) cout << " " << n;
+      sort(all(res));
+      cout << "ambiguous: ";
+      for (auto& i : res) cout << i << " ";
       cout << "\n";
     }
   }
