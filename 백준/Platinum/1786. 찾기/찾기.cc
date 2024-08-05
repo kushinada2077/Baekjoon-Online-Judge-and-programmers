5455fa550963bf56c802783a126d3091f8990220
kushinada <111179843+kushinada2077@users.noreply.github.com>
2024-08-05 20:01:21 +0900

    [Platinum V] Title: 찾기, Time: 44 ms, Memory: 9432 KB -BaekjoonHub

--- 백준/Platinum/1786. 찾기/찾기.cc
+++ 백준/Platinum/1786. 찾기/찾기.cc
@@ -1,43 +1,51 @@
-
-#define PAIR pair<long long, int>
-#define ll long long
 #include <algorithm>
+#include <climits>
+#include <deque>
 #include <iostream>
+#include <numeric>
+#include <queue>
+#include <tuple>
+#include <unordered_map>
 #include <vector>
+#define fastio cin.tie(0)->sync_with_stdio(0);
+#define for_in(n) for (int i = 0; i < n; ++i)
+#define si(x) int(x.size())
+#define all(x) (x).begin(), (x).end()
+#define pb(...) push_back(__VA_ARGS__)
+#define X first
+#define Y second
+using ll = long long;
 using namespace std;
 
-vector<int> failure(string& s) {
-  vector<int> f(s.size());
+vector<int> failure(const string& s) {
+  vector<int> f(si(s));
   int j = 0;
-  for (int i = 1; i < s.size(); i++) {
-    while (j > 0 && s[i] != s[j]) j = f[j - 1];
+  for (int i = 1; i < si(s); ++i) {
+    while (j && s[i] != s[j]) j = f[j - 1];
     if (s[i] == s[j]) f[i] = ++j;
   }
-
   return f;
 }
-
-int main() {
-  ios::sync_with_stdio(0);
-  cin.tie(0);
-
-  string s, p;
-  getline(cin, s);
-  getline(cin, p);
+vector<int> kmp(const string& s, const string& p) {
   vector<int> f = failure(p);
-  vector<int> ans;
+  vector<int> ret;
   int j = 0;
-
-  for (int i = 0; i < s.size(); i++) {
-    while (j > 0 && s[i] != p[j]) j = f[j - 1];
+  for (int i = 0; i < si(s); ++i) {
+    while (j && s[i] != p[j]) j = f[j - 1];
     if (s[i] == p[j]) j++;
-
-    if (j == p.size()) {
-      ans.push_back(i - j + 2);
+    if (j == si(p)) {
+      ret.pb(i - si(p) + 2);
       j = f[j - 1];
     }
   }
-
-  cout << ans.size() << "\n";
-  for (auto n : ans) cout << n << " ";
+  return ret;
+}
+int main() {
+  fastio;
+  string s, p;
+  getline(cin, s);
+  getline(cin, p);
+  vector<int> ans = kmp(s, p);
+  cout << si(ans) << "\n";
+  for (auto& i : ans) cout << i << " ";
 }
