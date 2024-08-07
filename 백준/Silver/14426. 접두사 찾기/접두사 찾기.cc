10366b73558f68856305d5f7ee7a315645369cef
kushinada <111179843+kushinada2077@users.noreply.github.com>
2024-08-07 14:49:43 +0900

    [Silver I] Title: 접두사 찾기, Time: 72 ms, Memory: 509836 KB -BaekjoonHub

--- 백준/Silver/14426. 접두사 찾기/접두사 찾기.cc
+++ 백준/Silver/14426. 접두사 찾기/접두사 찾기.cc
@@ -1,46 +1,54 @@
-#define PAIR pair<long long, int>
-#define ll long long
 #include <algorithm>
+#include <climits>
+#include <deque>
 #include <iostream>
-#include <unordered_set>
+#include <numeric>
+#include <queue>
+#include <tuple>
+#include <unordered_map>
 #include <vector>
+#define fastio cin.tie(0)->sync_with_stdio(0);
+#define FOR(n) for (int i = 0; i < n; ++i)
+#define si(x) int(x.size())
+#define all(x) (x).begin(), (x).end()
+#define pb(...) push_back(__VA_ARGS__)
+#define X first
+#define Y second
+using ll = long long;
 using namespace std;
 
 const int ROOT = 1;
 int unused = 2;
-const int MX = 10000 * 500 + 5;
+const int MX = 500 * 10000 + 5;
 int nxt[MX][26];
-
 int c2i(char c) { return c - 'a'; }
-void insert(string& s) {
+void insert(const string& s) {
   int cur = ROOT;
-  for (auto c : s) {
-    if (!nxt[cur][c2i(c)]) nxt[cur][c2i(c)] = unused++;
+  for (auto& c : s) {
+    if (nxt[cur][c2i(c)] == 0) nxt[cur][c2i(c)] = unused++;
     cur = nxt[cur][c2i(c)];
   }
 }
-bool find(string& s) {
+bool chk(const string& s) {
   int cur = ROOT;
-  for (auto c : s) {
-    if (!nxt[cur][c2i(c)]) return false;
+  for (auto& c : s) {
+    if (nxt[cur][c2i(c)] == 0) return false;
     cur = nxt[cur][c2i(c)];
   }
   return true;
 }
-
 int main() {
-  ios::sync_with_stdio(0);
-  cin.tie(0);
-  int n, m, cnt = 0;
+  fastio;
+  int n, m, ans = 0;
   string s;
   cin >> n >> m;
-  while (n--) {
+  FOR(n) {
     cin >> s;
     insert(s);
   }
-  while (m--) {
+  FOR(m) {
     cin >> s;
-    if (find(s)) cnt++;
+    ans += chk(s);
   }
-  cout << cnt;
+  cout << ans << "\n";
 }
