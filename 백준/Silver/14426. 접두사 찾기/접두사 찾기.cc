60f5cb9a559e9b4decd25a50b9d40adca79b0251
kushinada <111179843+kushinada2077@users.noreply.github.com>
2024-08-07 15:04:25 +0900

    [Silver I] Title: 접두사 찾기, Time: 48 ms, Memory: 7492 KB -BaekjoonHub

--- 백준/Silver/14426. 접두사 찾기/접두사 찾기.cc
+++ 백준/Silver/14426. 접두사 찾기/접두사 찾기.cc
@@ -8,7 +8,7 @@
 #include <unordered_map>
 #include <vector>
 #define fastio cin.tie(0)->sync_with_stdio(0);
-#define FOR(n) for (int i = 0; i < n; ++i)
+#define for_in(n) for (int i = 0; i < n; ++i)
 #define si(x) int(x.size())
 #define all(x) (x).begin(), (x).end()
 #define pb(...) push_back(__VA_ARGS__)
@@ -17,24 +17,10 @@
 using ll = long long;
 using namespace std;
 
-const int ROOT = 1;
-int unused = 2;
-const int MX = 500 * 10000 + 5;
-int nxt[MX][26];
-int c2i(char c) { return c - 'a'; }
-void insert(const string& s) {
-  int cur = ROOT;
-  for (auto& c : s) {
-    if (nxt[cur][c2i(c)] == 0) nxt[cur][c2i(c)] = unused++;
-    cur = nxt[cur][c2i(c)];
-  }
-}
-bool chk(const string& s) {
-  int cur = ROOT;
-  for (auto& c : s) {
-    if (nxt[cur][c2i(c)] == 0) return false;
-    cur = nxt[cur][c2i(c)];
-  }
+bool f(const string& a, const string& b) { return a < b; }
+bool chk(const string& a, const string& b) {
+  for (int i = 0; i < si(b); ++i)
+    if (a[i] != b[i]) return false;
   return true;
 }
 int main() {
@@ -42,13 +28,18 @@ int main() {
   int n, m, ans = 0;
   string s;
   cin >> n >> m;
-  FOR(n) {
+  vector<string> a(n);
+  for (int i = 0; i < n; ++i) cin >> a[i];
+  sort(all(a));
+  for (int i = 0; i < m; ++i) {
     cin >> s;
-    insert(s);
-  }
-  FOR(m) {
-    cin >> s;
-    ans += chk(s);
+    int lo = -1, hi = n - 1;
+    while (lo + 1 < hi) {
+      int mid = (lo + hi) / 2;
+      if (f(a[mid], s)) lo = mid;
+      else hi = mid;
+    }
+    ans += chk(a[hi], s);
   }
   cout << ans << "\n";
 }
