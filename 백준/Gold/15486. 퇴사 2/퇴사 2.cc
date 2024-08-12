3f6beb4468cfa919e243efaaf02d08919d9ad076
kushinada <111179843+kushinada2077@users.noreply.github.com>
2024-08-12 20:47:58 +0900

    [Gold V] Title: 퇴사 2, Time: 216 ms, Memory: 19600 KB -BaekjoonHub

--- 백준/Gold/15486. 퇴사 2/퇴사 2.cc
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.cc
@@ -1,33 +1,32 @@
 #include <algorithm>
+#include <climits>
+#include <deque>
 #include <iostream>
-#include <stack>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <tuple>
 #include <vector>
-#define ll long long
 #define fastio cin.tie(0)->sync_with_stdio(0);
-#define FOR_IN_1(n) for (int i = 0; i < n; ++i)
-#define FOR_IN_2(i, n) for (int i = 0; i < n; ++i)
-#define FOR_IN_3(i, m, n) for (int i = m; i < n; ++i)
-#define GET_MACRO(_1, _2, _3, NAME, ...) NAME
-#define for_in(...) GET_MACRO(__VA_ARGS__, FOR_IN_3, FOR_IN_2, FOR_IN_1)(__VA_ARGS__)
+#define for_in(n) for (int i = 0; i < n; ++i)
 #define si(x) int(x.size())
 #define all(x) (x).begin(), (x).end()
-#define pb(x) push_back(x)
+#define pb(...) push_back(__VA_ARGS__)
+#define X first
+#define Y second
+#define ROOT 1
+using ll = long long;
 using namespace std;
 
-// d[i] = i - 1번째 날까지 상담했을때의 최대 수익
 int main() {
   fastio;
   int n;
   cin >> n;
-  vector<int> t(n + 1), p(n + 1), d(n + 2);
-  for_in(n) {
-    cin >> t[i + 1];
-    cin >> p[i + 1];
+  vector<int> t(n + 1), p(n + 1), dp(n + 80);
+  for (int i = 1; i <= n; ++i) cin >> t[i] >> p[i];
+  for (int i = n; i != 0; --i) {
+    dp[i] = dp[i + 1];
+    if (i + t[i] <= n + 1 && dp[i] < dp[i + t[i]] + p[i]) dp[i] = dp[i + t[i]] + p[i];
   }
-  for_in(i, 1, n + 1) {
-    d[i] = max(d[i], d[i - 1]);
-    int nxt = i + t[i];
-    if (nxt - 1 <= n) d[nxt] = max(d[nxt], d[i] + p[i]);
-  }
-  cout << max(d[n], d[n + 1]);
+  cout << dp[1] << "\n";
 }
