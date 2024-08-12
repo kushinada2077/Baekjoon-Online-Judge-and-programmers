1cc2757637e7460e549c86806ca807bc9ea34955
kushinada <111179843+kushinada2077@users.noreply.github.com>
2024-08-12 21:22:09 +0900

    [Silver II] Title: 나무 자르기, Time: 140 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.cc
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.cc
@@ -1,36 +1,42 @@
-#define PAIR pair<long long, int>
-#define ll long long
 #include <algorithm>
+#include <climits>
+#include <deque>
 #include <iostream>
+#include <map>
+#include <numeric>
 #include <queue>
-#include <set>
+#include <tuple>
 #include <vector>
+#define fastio cin.tie(0)->sync_with_stdio(0);
+#define for_in(n) for (int i = 0; i < n; ++i)
+#define si(x) int(x.size())
+#define all(x) (x).begin(), (x).end()
+#define pb(...) push_back(__VA_ARGS__)
+#define X first
+#define Y second
+#define ROOT 1
+using ll = long long;
 using namespace std;
 
-int n;
-ll m, lo, hi = 1e9, h;
-vector<ll> trees;
-
+int n, m;
+bool f(int x, vector<int>& a) {
+  ll tot = 0;
+  for (auto& i : a) {
+    tot += 1ll * (i > x ? i - x : 0);
+    if (tot >= m) return true;
+  }
+  return tot >= m;
+}
 int main() {
-  ios::sync_with_stdio(0);
-  cin.tie(0);
+  fastio;
   cin >> n >> m;
-  while (n--) {
-    cin >> h;
-    trees.push_back(h);
-  }
-
+  vector<int> a(n);
+  for (auto& i : a) cin >> i;
+  int lo = 0, hi = 2e9 + 5;
   while (lo + 1 < hi) {
-    ll mid = (lo + hi) / 2;
-    ll sum = 0;
-    for (auto hei : trees) {
-      sum += hei - mid > 0 ? hei - mid : 0;
-    }
-    if (sum >= m)
-      lo = mid;
-    else
-      hi = mid;
+    int mid = (lo + hi) / 2;
+    if (f(mid, a)) lo = mid;
+    else hi = mid;
   }
-
-  cout << lo;
+  cout << lo << "\n";
 }
