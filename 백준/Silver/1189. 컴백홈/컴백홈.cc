b52494fcda6bf3cf3b485b8af8a96139646735a8
kushinada <111179843+kushinada2077@users.noreply.github.com>
2024-08-13 16:00:12 +0900

    [Silver I] Title: 컴백홈, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/1189. 컴백홈/컴백홈.cc
+++ 백준/Silver/1189. 컴백홈/컴백홈.cc
@@ -2,10 +2,10 @@
 #include <climits>
 #include <deque>
 #include <iostream>
+#include <map>
 #include <numeric>
 #include <queue>
 #include <tuple>
-#include <unordered_set>
 #include <vector>
 #define fastio cin.tie(0)->sync_with_stdio(0);
 #define for_in(n) for (int i = 0; i < n; ++i)
@@ -14,26 +14,25 @@
 #define pb(...) push_back(__VA_ARGS__)
 #define X first
 #define Y second
+#define ROOT 1
 using ll = long long;
 using namespace std;
 
 int r, c, k, ans;
-string board[10];
-vector<vector<bool>> vis(10, vector<bool>(10, false));
-
-void f(int x, int y, int cnt) {
-  if (cnt > k) return;
-  if (x == 0 && y == c - 1 && cnt == k) {
-    ans++;
+string board[6];
+vector<vector<bool>> vis(6, vector<bool>(6, false));
+void dfs(int n, int x, int y) {
+  if (n == k - 1) {
+    if (x == 0 && y == c - 1) ans++;
     return;
   }
   for (int dir = 0; dir < 4; ++dir) {
     int nx = x + "2101"[dir] - '1';
     int ny = y + "1210"[dir] - '1';
     if (nx < 0 || nx >= r || ny < 0 || ny >= c) continue;
-    if (board[nx][ny] == 'T' || vis[nx][ny]) continue;
+    if (vis[nx][ny] || board[nx][ny] == 'T') continue;
     vis[nx][ny] = true;
-    f(nx, ny, cnt + 1);
+    dfs(n + 1, nx, ny);
     vis[nx][ny] = false;
   }
 }
@@ -42,6 +41,6 @@ int main() {
   cin >> r >> c >> k;
   for (int i = 0; i < r; ++i) cin >> board[i];
   vis[r - 1][0] = true;
-  f(r - 1, 0, 1);
+  dfs(0, r - 1, 0);
   cout << ans << "\n";
 }
