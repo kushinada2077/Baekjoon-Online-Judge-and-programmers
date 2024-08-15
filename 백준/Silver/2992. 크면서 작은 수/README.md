4e85e72ded82d860e15056200a06e8622480f987
kushinada <111179843+kushinada2077@users.noreply.github.com>
2024-08-16 04:08:51 +0900

    [Silver III] Title: 크면서 작은 수, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/2992. 크면서 작은 수/README.md
@@ -0,0 +1,32 @@
+# [Silver III] 크면서 작은 수 - 2992 
+
+[문제 링크](https://www.acmicpc.net/problem/2992) 
+
+### 성능 요약
+
+메모리: 2024 KB, 시간: 0 ms
+
+### 분류
+
+백트래킹, 브루트포스 알고리즘
+
+### 제출 일자
+
+2024년 8월 16일 04:08:42
+
+### 문제 설명
+
+<p>Your program will be given an integer X. Find the smallest number larger than X consisting of the same digits as X. </p>
+
+### 입력 
+
+ <p>The first line of input contains the integer X (1 ≤ X ≤ 999999). </p>
+
+<p>The first digit in X will not be a zero. </p>
+
+### 출력 
+
+ <p>Output the result on a single line. If there is no such number, output 0. </p>
+
+<p> </p>
+
---
+++ 백준/Silver/2992. 크면서 작은 수/크면서 작은 수.cc
@@ -0,0 +1,30 @@
+#include <algorithm>
+#include <climits>
+#include <deque>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <tuple>
+#include <vector>
+#define fastio cin.tie(0)->sync_with_stdio(0);
+#define for_in(n) for (int i = 0; i < n; ++i)
+#define si(x) int(x.size())
+#define all(x) (x).begin(), (x).end()
+#define pb(...) push_back(__VA_ARGS__)
+#define X first
+#define Y second
+#define ROOT 1
+using ll = long long;
+using namespace std;
+
+int main() {
+  fastio;
+  string s, c;
+  cin >> s;
+  c = s;
+  sort(all(c));
+  next_permutation(all(s));
+  if (c == s) cout << "0\n";
+  else cout << s << "\n";
+}
