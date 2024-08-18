aa42ff69d216f54a95ae7630346c400428121eca
kushinada <111179843+kushinada2077@users.noreply.github.com>
2024-08-18 23:06:39 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 36 ms, Memory: 2916 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.cc
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.cc
@@ -1,24 +1,36 @@
-#define ll long long
 #include <algorithm>
+#include <climits>
+#include <deque>
 #include <iostream>
-#include <unordered_set>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <tuple>
 #include <vector>
+#define fastio cin.tie(0)->sync_with_stdio(0);
+#define for_in(n) for (int i = 0; i < n; ++i)
+#define si(x) int(x.size())
+#define all(x) (x).begin(), (x).end()
+#define pb(...) push_back(__VA_ARGS__)
+#define X first
+#define Y second
+#define ROOT 1
+using ll = long long;
 using namespace std;
 
-int N, M, n;
-ll d[100005];
 int main() {
-  ios::sync_with_stdio(0);
-  cin.tie(0);
-  cin >> N >> M;
-  ll sum = 0;
-  for (int i = 1; i < N + 1; ++i) {
-    cin >> n;
-    sum += n;
-    d[i] = sum;
+  fastio;
+  int n, m, a, b, x;
+  cin >> n >> m;
+  vector<int> r = {0};
+  for (int i = 0; i < n; ++i) {
+    cin >> x;
+    r.pb(r.back() + x);
   }
-  while (M--) {
-    cin >> N >> n;
-    cout << d[n] - d[N - 1] << "\n";
+
+  while (m--) {
+    cin >> a >> b;
+    cout << r[b] - r[a - 1] << "\n";
   }
 }
