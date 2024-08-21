1601967d604ab19aac165941891976964cfe13fe
kushinada <111179843+kushinada2077@users.noreply.github.com>
2024-08-21 09:51:54 +0900

    [Gold V] Title: 선 긋기, Time: 336 ms, Memory: 14440 KB -BaekjoonHub

---
+++ 백준/Gold/2170. 선 긋기/선 긋기.cc
@@ -0,0 +1,49 @@
+#include <algorithm>
+#include <climits>
+#include <deque>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <tuple>
+#include <vector>
+#define fastio cin.tie(0)->sync_with_stdio(0);
+#define for_in(n) for (int i = 0; i < n; ++i)
+#define si(x) int(x.size())
+#define all(x) (x).begin(), (x).end()
+#define pb(...) push_back(__VA_ARGS__)
+#define X first
+#define Y second
+#define ROOT 1
+using ll = long long;
+using namespace std;
+
+int main() {
+  fastio;
+  int n, x, y;
+  cin >> n;
+  vector<pair<int, int>> a;
+  for (int i = 0; i < n; ++i) {
+    cin >> x >> y;
+    a.pb({x, y});
+  }
+  sort(all(a));
+  ll ans = 0;
+  int l, r;
+  tie(l, r) = a[0];
+  for (int i = 1; i < n; ++i) {
+    auto [nl, nr] = a[i];
+    if (r < nl || nr < l) {
+      ans += r - l;
+      l = nl;
+      r = nr;
+    } else {
+      l = min(l, nl);
+      r = max(r, nr);
+    }
+  }
+  ans += r - l;
+  cout << ans << "\n";
+}
