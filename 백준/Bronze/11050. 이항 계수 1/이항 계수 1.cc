0c6304bbb7411af391e10e25f8762c7bb8f57266
kushinada <111179843+kushinada2077@users.noreply.github.com>
2024-08-25 00:39:41 +0900

    [Bronze I] Title: 이항 계수 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.cc
@@ -0,0 +1,36 @@
+#include <algorithm>
+#include <climits>
+#include <deque>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+#define PATH "/Users/leedongha/Downloads/PS/input.txt"
+#define fastio cin.tie(0)->sync_with_stdio(0);
+#define for_in(n) for (int i = 0; i < n; ++i)
+#define si(x) int(x.size())
+#define all(x) (x).begin(), (x).end()
+#define pb(...) push_back(__VA_ARGS__)
+#define X first
+#define Y second
+#define ROOT 1
+using ll = long long;
+using namespace std;
+
+int f(int n) {
+  if (n == 0) return 1;
+  return n * f(n - 1);
+}
+
+int main() {
+  fastio;
+  int n, k;
+  cin >> n >> k;
+  cout << f(n) / (f(n - k) * f(k)) << "\n";
+}
