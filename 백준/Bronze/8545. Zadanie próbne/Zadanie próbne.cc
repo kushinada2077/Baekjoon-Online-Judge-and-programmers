6e0de6683c268a31218d6da85a0b8b845576ac6c
kushinada <111179843+kushinada2077@users.noreply.github.com>
2024-08-31 03:44:36 +0900

    [Bronze V] Title: Zadanie próbne, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/8545. Zadanie próbne/Zadanie próbne.cc
@@ -0,0 +1,32 @@
+#include <algorithm>
+#include <climits>
+#include <deque>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+#define PATH "/Users/leedongha/Downloads/PS/input.txt"
+#define fastio cin.tie(0)->sync_with_stdio(0);
+#define for_in(n) for (int i = 0; i < n; ++i)
+#define si(x) int(x.size())
+#define all(x) (x).begin(), (x).end()
+#define pb(...) push_back(__VA_ARGS__)
+#define X first
+#define Y second
+#define ROOT 1
+using ll = long long;
+using namespace std;
+
+int main() {
+  fastio;
+  string s;
+  cin >> s;
+  reverse(all(s));
+  cout << s << "\n";
+}
