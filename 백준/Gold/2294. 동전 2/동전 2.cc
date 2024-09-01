a6a15ac2610ae68ed20a1a538f4026f2d44067ff
kushinada <111179843+kushinada2077@users.noreply.github.com>
2024-09-01 16:26:09 +0900

    [Gold V] Title: 동전 2, Time: 0 ms, Memory: 2060 KB -BaekjoonHub

--- 백준/Gold/2294. 동전 2/동전 2.cc
+++ 백준/Gold/2294. 동전 2/동전 2.cc
@@ -1,36 +1,42 @@
-#define ll long long
 #include <algorithm>
+#include <climits>
+#include <deque>
 #include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <tuple>
 #include <unordered_map>
 #include <unordered_set>
 #include <vector>
+#define PATH "/Users/leedongha/Downloads/PS/input.txt"
+#define fastio cin.tie(0)->sync_with_stdio(0);
+#define for_in(n) for (int i = 0; i < n; ++i)
+#define si(x) int(x.size())
+#define all(x) (x).begin(), (x).end()
+#define pb(...) push_back(__VA_ARGS__)
+#define X first
+#define Y second
+#define ROOT 1
+#define INF 0x3f3f3f3f
+using ll = long long;
 using namespace std;
 
-const int INF = 0x3f3f3f3f;
+int dp[10005];
 int n, k;
-int coin[101];
-int d[10005];
-
-int f(int k) {
-  if (k <= 0) return INF;
-  if (d[k] != -1) return d[k];
-
-  int result = INF;
-  for (int i = 0; i < n; ++i) result = min(result, f(k - coin[i]) + 1);
-  d[k] = result;
-  return result;
-}
 int main() {
-  ios::sync_with_stdio(0);
-  cin.tie(0);
+  fastio;
   cin >> n >> k;
-  fill(d, d + 10005, -1);
-  for (int i = 0; i < n; ++i) {
-    cin >> coin[i];
-    if (coin[i] > 10000) continue;
-    d[coin[i]] = 1;
+  vector<int> a(n + 1);
+  for (int i = 1; i <= n; ++i) cin >> a[i];
+  for (int i = 1; i <= k; ++i) {
+    dp[i] = INF;
+    for (int j = 1; j <= n; ++j) {
+      if (i - a[j] >= 0) dp[i] = min(dp[i], dp[i - a[j]] + 1);
+    }
   }
-  int ans = f(k);
-  if (ans == INF) ans = -1;
-  cout << ans;
+  if (dp[k] == INF) cout << "-1\n";
+  else cout << dp[k] << "\n";
 }
