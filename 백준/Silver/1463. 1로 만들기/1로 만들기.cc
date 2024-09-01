45079167b3dc95440b751a491d046aa28021e6ce
kushinada <111179843+kushinada2077@users.noreply.github.com>
2024-09-01 13:55:09 +0900

    [Silver III] Title: 1로 만들기, Time: 4 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.cc
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.cc
@@ -1,28 +1,41 @@
-#define ll long long
 #include <algorithm>
+#include <climits>
+#include <deque>
 #include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <tuple>
 #include <unordered_map>
 #include <unordered_set>
 #include <vector>
+#define PATH "/Users/leedongha/Downloads/PS/input.txt"
+#define fastio cin.tie(0)->sync_with_stdio(0);
+#define for_in(n) for (int i = 0; i < n; ++i)
+#define si(x) int(x.size())
+#define all(x) (x).begin(), (x).end()
+#define pb(...) push_back(__VA_ARGS__)
+#define X first
+#define Y second
+#define ROOT 1
+using ll = long long;
 using namespace std;
 
-int ans = 0x3f3f3f3f;
-int d[1000005];
-void sol(int x, int cnt) {
-  if (d[x] && d[x] < cnt) return;
-  d[x] = cnt;
-  if (x == 1) return;
+const int INF = 0x3f3f3f3f;
 
-  if (x % 3 == 0) sol(x / 3, cnt + 1);
-  if (x % 2 == 0) sol(x / 2, cnt + 1);
-  sol(x - 1, cnt + 1);
-}
+vector<int> dp(1000005, INF);
 
 int main() {
-  ios::sync_with_stdio(0);
-  cin.tie(0);
+  fastio;
   int x;
   cin >> x;
-  sol(x, 0);
-  cout << d[1];
+  dp[1] = 0;
+  for (int i = 1; i < x; ++i) {
+    dp[i + 1] = min(dp[i + 1], dp[i] + 1);
+    if (2 * i <= x) dp[2 * i] = min(dp[2 * i], dp[i] + 1);
+    if (3 * i <= x) dp[3 * i] = min(dp[3 * i], dp[i] + 1);
+  }
+  cout << dp[x] << "\n";
 }
