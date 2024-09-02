f4536d7ba91dc3c6bbec0b9984bfc22e6214020e
kushinada <111179843+kushinada2077@users.noreply.github.com>
2024-09-02 19:25:44 +0900

    [Platinum III] Title: 북서풍, Time: 388 ms, Memory: 5224 KB -BaekjoonHub

--- 백준/Platinum/5419. 북서풍/북서풍.cc
+++ 백준/Platinum/5419. 북서풍/북서풍.cc
@@ -8,7 +8,10 @@
 #include <set>
 #include <stack>
 #include <tuple>
+#include <unordered_map>
+#include <unordered_set>
 #include <vector>
+#define PATH "/Users/leedongha/Downloads/PS/input.txt"
 #define fastio cin.tie(0)->sync_with_stdio(0);
 #define for_in(n) for (int i = 0; i < n; ++i)
 #define si(x) int(x.size())
@@ -17,55 +20,48 @@
 #define X first
 #define Y second
 #define ROOT 1
+#define INF 0x3f3f3f3f
 using ll = long long;
 using namespace std;
 
 const int _size = 1 << 17;
 int seg[_size << 1];
-vector<pair<int, int>> coor;
-void init() {
-  fill(seg, seg + (_size << 1), 0);
-  coor.clear();
-}
-void update(int i, int x) {
-  i += _size;
-  seg[i] += x;
-  while (i > 1) {
-    i >>= 1;
-    seg[i] = seg[2 * i] + seg[2 * i + 1];
-  }
-}
-int sum(int i, int st, int en, int l, int r) {
-  if (r <= st || en <= l) return 0;
-  if (l <= st && en <= r) return seg[i];
+int sum(int idx, int st, int en, int l, int r) {
+  if (en <= l || r <= st) return 0;
+  if (l <= st && en <= r) return seg[idx];
   int mid = (st + en) / 2;
-  return sum(2 * i, st, mid, l, r) + sum(2 * i + 1, mid, en, l, r);
+  return sum(idx << 1, st, mid, l, r) + sum(idx << 1 | 1, mid, en, l, r);
+}
+void update(int idx, int x) {
+  idx += _size;
+  seg[idx] += x;
+  while (idx > 1) {
+    idx >>= 1;
+    seg[idx] = seg[idx << 1] + seg[idx << 1 | 1];
+  }
 }
 int main() {
   fastio;
-  int t, x, y, n;
+  int t, n, x, y;
   cin >> t;
   while (t--) {
-    init();
-    vector<int> uni;
-    ll ans = 0;
     cin >> n;
+    vector<pair<int, int>> coor;
+    vector<int> comp;
+    fill(seg, seg + (_size << 1), 0);
+    ll ans = 0;
     for (int i = 0; i < n; ++i) {
       cin >> x >> y;
-      coor.pb({x, y});
-      uni.pb(y);
+      coor.pb({x, -y});
+      comp.pb(y);
     }
-    sort(all(uni));
-    uni.erase(unique(all(uni)), uni.end());
-    for (int i = 0; i < n; ++i) {
-      coor[i].Y = -(lower_bound(all(uni), coor[i].Y) - uni.begin());
-    }
+    sort(all(comp));
+    comp.erase(unique(all(comp)), comp.end());
     sort(all(coor));
+    for (int i = 0; i < n; ++i) coor[i].Y = lower_bound(all(comp), -coor[i].Y) - comp.begin();
     for (auto [x, y] : coor) {
-      y *= -1;
-      int res = sum(1, 0, _size, y, _size);
+      ans += 1ll * sum(ROOT, 0, _size, y, _size);
       update(y, 1);
-      ans += 1ll * res;
     }
     cout << ans << "\n";
   }
