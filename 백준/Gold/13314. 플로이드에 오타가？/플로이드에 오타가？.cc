dd4c77c0d6c65d7567cf0029e19e102348686171
kushinada <111179843+kushinada2077@users.noreply.github.com>
2024-09-02 20:37:41 +0900

    [Gold III] Title: 플로이드에 오타가?, Time: 0 ms, Memory: 2152 KB -BaekjoonHub

---
+++ 백준/Gold/13314. 플로이드에 오타가？/플로이드에 오타가？.cc
@@ -0,0 +1,42 @@
+#include <algorithm>
+#include <climits>
+#include <deque>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+#define PATH "/Users/leedongha/Downloads/PS/input.txt"
+#define fastio cin.tie(0)->sync_with_stdio(0);
+#define for_in(n) for (int i = 0; i < n; ++i)
+#define si(x) int(x.size())
+#define all(x) (x).begin(), (x).end()
+#define pb(...) push_back(__VA_ARGS__)
+#define X first
+#define Y second
+#define ROOT 1
+#define INF 0x3f3f3f3f
+using ll = long long;
+using namespace std;
+
+int main() {
+  fastio;
+  int n = 100;
+  cout << n << "\n";
+  vector<vector<int>> d(105, vector<int>(105, 1000));
+  for (int i = 1; i <= n; ++i) {
+    for (int j = 1; j <= n; ++j) {
+      if (i == n || j == n) d[i][j] = 499;
+      if (i == j) d[i][j] = 0;
+    }
+  }
+  for (int i = 1; i <= n; ++i) {
+    for (int j = 1; j <= n; ++j) cout << d[i][j] << " ";
+    cout << "\n";
+  }
+}
