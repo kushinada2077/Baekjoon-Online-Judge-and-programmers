1a4695f93aeceecbf3dcc3c6a75062d14e7e72c1
kushinada <111179843+kushinada2077@users.noreply.github.com>
2024-09-03 02:52:25 +0900

    [Gold III] Title: 수상 택시, Time: 88 ms, Memory: 8296 KB -BaekjoonHub

--- 백준/Gold/2836. 수상 택시/수상 택시.cc
+++ 백준/Gold/2836. 수상 택시/수상 택시.cc
@@ -26,7 +26,6 @@ using namespace std;
 
 int main() {
   fastio;
-  // freopen(PATH, "r", stdin);
   int n, m, u, v;
   cin >> n >> m;
   vector<pair<int, int>> a;
@@ -36,21 +35,15 @@ int main() {
   }
   sort(all(a));
   int cx = -INF, cy = -INF;
-  ll ans = 0;
-  vector<int> r;
+  ll ans = m;
   for (int i = 0; i < si(a); ++i) {
     auto [nx, ny] = a[i];
     if (cy < nx) {
       ans += 2ll * (1ll * cy - cx);
-      r.pb(cy);
       cx = nx;
       cy = ny;
     } else if (cy < ny) cy = ny;
   }
   ans += 2ll * (1ll * cy - cx);
-  r.pb(cy);
-  r.pb(m);
-  r[0] = 0;
-  for (int i = 0; i < si(r) - 1; ++i) ans += 1ll * r[i + 1] - r[i];
   cout << ans << "\n";
 }
