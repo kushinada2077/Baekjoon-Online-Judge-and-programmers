4141b4e6aa0fba76b703fca8341aa9663d76f203
kushinada <111179843+kushinada2077@users.noreply.github.com>
2024-09-14 02:08:44 +0900

    [Bronze V] Title: 문자열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/9086. 문자열/문자열.cc
@@ -0,0 +1,27 @@
+#include <algorithm>
+#include <climits>
+#include <iostream>
+#include <vector>
+#define PATH "/Users/leedongha/Downloads/PS/input.txt"
+#define fastio cin.tie(0)->sync_with_stdio(0);
+#define for_in(n) for (int i = 0; i < n; ++i)
+#define si(x) int(x.size())
+#define all(x) (x).begin(), (x).end()
+#define pb(...) push_back(__VA_ARGS__)
+#define X first
+#define Y second
+#define ROOT 1
+#define INF 0x3f3f3f3f
+using ll = long long;
+using namespace std;
+
+int main() {
+  fastio;
+  int t;
+  cin >> t;
+  string s;
+  while (t--) {
+    cin >> s;
+    cout << s.front() << s.back() << "\n";
+  }
+}
