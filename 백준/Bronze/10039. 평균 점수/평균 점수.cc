8d68a149c88789f5fcc0cde7cdbc723870174b26
kushinada <111179843+kushinada2077@users.noreply.github.com>
2024-09-22 00:49:11 +0900

    [Bronze IV] Title: 평균 점수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10039. 평균 점수/평균 점수.cc
@@ -0,0 +1,28 @@
+#include <algorithm>
+#include <iostream>
+#include <queue>
+#include <stack>
+#include <vector>
+#define PATH "/Users/leedongha/Downloads/PS/input.txt"
+#define fastio cin.tie(0)->sync_with_stdio(0);
+#define for_in(n) for (int i = 0; i < n; ++i)
+#define si(x) int(x.size())
+#define all(x) (x).begin(), (x).end()
+#define pb(...) push_back(__VA_ARGS__)
+#define X first
+#define Y second
+#define ROOT 1
+#define INF 0x3f3f3f3f
+using ll = long long;
+using namespace std;
+
+int main() {
+  fastio;
+  int sum = 0, x;
+  for (int i = 0; i < 5; ++i) {
+    cin >> x;
+    if (x < 40) x = 40;
+    sum += x;
+  }
+  cout << sum / 5 << "\n";
+}
