10c9d5ce9bd3f493367b27b0b0e7e4e326b746a5
kushinada <111179843+kushinada2077@users.noreply.github.com>
2024-10-04 18:32:44 +0900

    [Silver IV] Title: 상근이의 여행, Time: 16 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/9372. 상근이의 여행/상근이의 여행.cc
+++ 백준/Silver/9372. 상근이의 여행/상근이의 여행.cc
@@ -1,54 +1,29 @@
-#define TUPLE tuple<int, int, int>
 #include <algorithm>
 #include <iostream>
 #include <queue>
-#include <tuple>
+#include <set>
+#include <stack>
 #include <vector>
+#define PATH "/Users/leedongha/Downloads/PS/input.txt"
+#define fastio cin.tie(0)->sync_with_stdio(0);
+#define for_in(n) for (int i = 0; i < n; ++i)
+#define si(x) int(x.size())
+#define all(x) (x).begin(), (x).end()
+#define pb(...) push_back(__VA_ARGS__)
+#define X first
+#define Y second
+#define ROOT 1
+#define INF 0x3f3f3f3f
+using ll = long long;
 using namespace std;
 
-int T, n, m, a, b, ans, p[1001];
-
-int find(int v) {
-  if (p[v] < 0) return v;
-  return p[v] = find(p[v]);
-}
-
-bool is_diff_group(int a, int b) {
-  a = find(a);
-  b = find(b);
-  if (a == b) return 0;
-  if (p[a] == p[b]) p[a]--;
-  if (p[a] > p[b])
-    p[a] = b;
-  else
-    p[b] = a;
-
-  return 1;
-}
-
 int main() {
-  ios::sync_with_stdio(0);
-  cin.tie(0);
-
-  cin >> T;
-
-  while (T--) {
+  fastio;
+  int t, n, m, u, v;
+  cin >> t;
+  while (t--) {
     cin >> n >> m;
-    vector<pair<int, int>> edge;
-    fill(p, p + 1 + n, -1);
-    for (int i = 0; i < m; i++) {
-      cin >> a >> b;
-      edge.push_back({a, b});
-    }
-
-    for (int i = 0; i < m; i++) {
-      auto [a, b] = edge[i];
-      if (!is_diff_group(a, b)) continue;
-      ans++;
-      if (ans == n - 1) break;
-    }
-
-    cout << ans << "\n";
-    ans = 0;
+    for (int i = 0; i < m; ++i) cin >> u >> v;
+    cout << n - 1 << "\n";
   }
 }
