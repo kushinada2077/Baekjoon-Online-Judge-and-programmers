1d1aecc8ddc4bb9e59b27745c3d2fd29859407df
kushinada <111179843+kushinada2077@users.noreply.github.com>
2024-10-06 14:58:34 +0900

    [Platinum IV] Title: 단절점, Time: 24 ms, Memory: 7724 KB -BaekjoonHub

---
+++ 백준/Platinum/11266. 단절점/단절점.cc
@@ -0,0 +1,83 @@
+#include <algorithm>
+#include <iostream>
+#include <queue>
+#include <set>
+#include <stack>
+#include <vector>
+#define PATH "/Users/leedongha/Downloads/PS/input.txt"
+#define fastio cin.tie(0)->sync_with_stdio(0);
+#define for_in(n) for (int i = 0; i < n; ++i)
+#define si(x) int(x.size())
+#define all(x) (x).begin(), (x).end()
+#define pb(...) push_back(__VA_ARGS__)
+#define X first
+#define Y second
+#define ROOT 1
+#define INF 0x3f3f3f3f
+using ll = long long;
+using namespace std;
+
+const int MX = 10001;
+int n, m, u, v, cnt = 1, root;
+int dfsn[MX], chk[MX];
+vector<int> adj[MX], ans;
+vector<vector<pair<int, int>>> bcc;
+stack<pair<int, int>> s;
+
+int DFS(int cur, int prev = -1) {
+  int res = dfsn[cur] = cnt++;
+  bool f = 0;
+  for (auto& nxt : adj[cur]) {
+    if (nxt == prev) continue;
+    if (dfsn[cur] > dfsn[nxt]) s.push({cur, nxt});
+    if (dfsn[nxt] > 0) res = min(res, dfsn[nxt]);
+    else {
+      int tmp = DFS(nxt, cur);
+      res = min(res, tmp);
+      if (tmp >= dfsn[cur]) {
+        vector<pair<int, int>> cBcc;
+        while (1) {
+          auto [u, v] = s.top();
+          s.pop();
+          cBcc.pb({u, v});
+          if (u == cur && v == nxt) break;
+        }
+        bcc.pb(cBcc);
+      }
+    }
+  }
+  return res;
+}
+
+int main() {
+  fastio;
+  cin >> n >> m;
+  for (int i = 0; i < m; ++i) {
+    cin >> u >> v;
+    adj[u].pb(v);
+    adj[v].pb(u);
+  }
+
+  for (int i = 1; i <= n; ++i) {
+    if (dfsn[i] == 0) {
+      root = i;
+      DFS(i);
+    }
+  }
+
+  for (int i = 0; i < si(bcc); ++i) {
+    auto& c = bcc[i];
+    for (auto& [u, v] : c) {
+      if (chk[u] == 0) chk[u] = i + 1;
+      else if (chk[u] > 0 && chk[u] != i + 1) ans.pb(u);
+      if (chk[v] == 0) chk[v] = i + 1;
+      else if (chk[v] > 0 && chk[v] != i + 1) ans.pb(v);
+    }
+  }
+
+  sort(all(ans));
+  ans.erase(unique(all(ans)), ans.end());
+  cout << si(ans) << "\n";
+  for (auto& i : ans) cout << i << " ";
+  cout << "\n";
+}
