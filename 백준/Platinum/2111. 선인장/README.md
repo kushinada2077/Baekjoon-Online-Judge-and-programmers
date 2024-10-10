34a1fedc967dbcfa4b156eab80f1ba6f57691974
kushinada <111179843+kushinada2077@users.noreply.github.com>
2024-10-10 13:37:06 +0900

    [Platinum II] Title: 선인장, Time: 200 ms, Memory: 7420 KB -BaekjoonHub

---
+++ 백준/Platinum/2111. 선인장/README.md
@@ -0,0 +1,34 @@
+# [Platinum II] 선인장 - 2111 
+
+[문제 링크](https://www.acmicpc.net/problem/2111) 
+
+### 성능 요약
+
+메모리: 7420 KB, 시간: 200 ms
+
+### 분류
+
+임의 정밀도 / 큰 수 연산, 이중 연결 요소, 선인장, 그래프 이론
+
+### 제출 일자
+
+2024년 10월 10일 13:36:54
+
+### 문제 설명
+
+<p>Cactus is a connected undirected graph in which every edge lies on at most one simple cycle. Intuitively cactus is a generalization of a tree where some cycles are allowed. Your task first is to verify if the given graph is a cactus or not. Important diﬀerence between a cactus and a tree is that a cactus can have a number of spanning subgraphs that are also cactuses. The number of such subgraphs (including the graph itself) determines cactusness of a graph (this number is one for a cactus that is just a tree). The cactusness of a graph that is not a cactus is considered to be zero.</p>
+
+<p><img alt="" src="https://www.acmicpc.net/upload/images/cactus.png" style="height:255px; width:563px"></p>
+
+<p>The first graph on the picture is a cactus with cactusness 35. The second graph is not a cactus because edge (2, 3) lies on two cycles. The third graph is not a cactus because it is not connected.</p>
+
+### 입력 
+
+ <p>The first line of the input file contains two integer numbers n and m (1 ≤ n ≤ 20000, 0 ≤ m ≤ 1000). Here n is the number of vertices in the graph. Vertices are numbered from 1 to n. Edges of the graph are represented by a set of edge-distinct paths, where m is the number of such paths.</p>
+
+<p>Each of the following m lines contains a path in the graph. A path starts with an integer number k<sub>i</sub> (2 ≤ k<sub>i</sub> ≤ 1000) followed by k<sub>i</sub> integers from 1 to n. These k<sub>i</sub> integers represent vertices of a path. Path can go to the same vertex multiple times, but every edge is traversed exactly once in the whole input file. There are no multiedges in the graph (there is at most one edge between any two vertices).</p>
+
+### 출력 
+
+ <p>Write to the output file a single integer number — the cactusness of the given graph. Note that cactusness can be quite a large number.</p>
+
---
+++ 백준/Platinum/2111. 선인장/선인장.cc
@@ -0,0 +1,121 @@
+#include <algorithm>
+#include <deque>
+#include <iostream>
+#include <queue>
+#include <set>
+#include <stack>
+#include <vector>
+#define PATH "/Users/leedongha/Downloads/PS/input.txt"
+#define fastio cin.tie(0)->sync_with_stdio(0);
+#define rep(n) for (int i = 0; i < n; ++i)
+#define si(x) int(x.size())
+#define all(x) (x).begin(), (x).end()
+#define pb(...) push_back(__VA_ARGS__)
+#define X first
+#define Y second
+#define ROOT 1
+#define INF 0x3f3f3f3f
+using ll = long long;
+using namespace std;
+
+const int MX = 20001;
+int n, m, u, v, x, cnt, BN, cN;
+int dfsn[MX], bccSize[MX];
+vector<pair<int, int>> bcc[MX];
+vector<int> adj[MX];
+stack<pair<int, int>> s;
+vector<int> ans = {1};
+
+vector<int> mul(vector<int> A, int B) {
+  int asz = A.size(), rsz = asz, p = 0, n;
+  vector<int> result(rsz);
+  while (B) {
+    n = B % 10;
+    B /= 10;
+    result.resize(++rsz);
+    for (int i = 0; i < asz; i++) result[i + p] += A[i] * n;
+    p++;
+  }
+
+  for (int i = 0, n; i < rsz; i++) {
+    n = result[i] % 10;
+    result[i + 1] += (result[i] - n) / 10;
+    result[i] = n;
+  }
+
+  if (!result[rsz - 1]) result.resize(--rsz);
+
+  return result;
+}
+
+int dfs(int cur, int prev = -1) {
+  int res = dfsn[cur] = ++cnt;
+
+  for (auto& nxt : adj[cur]) {
+    if (nxt == prev) continue;
+    if (dfsn[cur] > dfsn[nxt]) s.push({cur, nxt});
+    if (dfsn[nxt] > 0) res = min(res, dfsn[nxt]);
+    else {
+      int tmp = dfs(nxt, cur);
+      res = min(res, tmp);
+      if (tmp >= dfsn[cur]) {
+        while (1) {
+          auto& [u, v] = s.top();
+          s.pop();
+          bcc[BN].pb({u, v});
+          bccSize[BN]++;
+          if (u == cur && v == nxt) break;
+        }
+        BN++;
+      }
+    }
+  }
+  return res;
+}
+
+int main() {
+  fastio;
+  cin >> n >> m;
+  rep(m) {
+    cin >> x >> u;
+    for (int j = 0; j < x - 1; ++j) {
+      cin >> v;
+      adj[u].pb(v);
+      adj[v].pb(u);
+      u = v;
+    }
+  }
+
+  for (int i = 1; i <= n; ++i) {
+    if (dfsn[i] == 0) {
+      dfs(i);
+      cN++;
+    }
+  }
+
+  if (cN > 1) {
+    cout << "0\n";
+    return 0;
+  }
+
+  for (int i = 0; i < BN; ++i) {
+    if (bccSize[i] < 3) continue;
+
+    set<int> tmp;
+    for (auto& [u, v] : bcc[i]) {
+      tmp.insert(u);
+      tmp.insert(v);
+    }
+
+    if (si(tmp) != bccSize[i]) {
+      cout << "0\n";
+      return 0;
+    }
+
+    int vn = bccSize[i] + 1;
+    ans = mul(ans, vn);
+  }
+
+  for (int i = si(ans) - 1; i >= 0; --i) cout << ans[i];
+  cout << "\n";
+}
