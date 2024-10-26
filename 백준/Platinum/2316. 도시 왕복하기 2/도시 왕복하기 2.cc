8b477adaccb0d4010f7715a2d17a24e80243a70e
kushinada <111179843+kushinada2077@users.noreply.github.com>
2024-10-26 19:36:47 +0900

    [Platinum III] Title: 도시 왕복하기 2, Time: 8 ms, Memory: 3892 KB -BaekjoonHub

---
+++ 백준/Platinum/2316. 도시 왕복하기 2/도시 왕복하기 2.cc
@@ -0,0 +1,93 @@
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <queue>
+#include <vector>
+#define PATH "/Users/leedongha/Downloads/PS/input.txt"
+#define L_PATH "input.txt"
+#define fastio cin.tie(0)->sync_with_stdio(0);
+#define rep(n) for (int i = 0; i < n; ++i)
+#define si(x) int(x.size())
+#define all(x) (x).begin(), (x).end()
+#define pb(...) push_back(__VA_ARGS__)
+#define X first
+#define Y second
+#define ROOT 1
+#define INF 0x3f3f3f3f
+using namespace std;
+using ll = long long;
+// using T = tuple<int, int, int>;
+using P = pair<int, int>;
+
+const int MX = 810, S = 1, T = 2;
+struct Edge {
+  int to, c, f;
+  Edge* dual;
+  Edge(int to, int c) : to(to), c(c), f(0), dual(nullptr) {}
+  int spare() { return c - f; }
+  void addFlow(int flow) {
+    f += flow;
+    dual->f -= flow;
+  }
+};
+
+int n, m;
+vector<Edge*> adj[MX];
+int main() {
+  fastio;
+  cin >> n >> m;
+  for (int i = 0, u = i * 2, v = u + 1; i < n; ++i, u = i * 2, v = u + 1) {
+    Edge *e1 = new Edge(v, 1), *e2 = new Edge(u, 0);
+    e1->dual = e2;
+    e2->dual = e1;
+    adj[u].pb(e1);
+    adj[v].pb(e2);
+  }
+  for (int u, v, i{}; i < m; ++i) {
+    cin >> u >> v;
+    u--;
+    v--;
+    u *= 2;
+    v *= 2;
+    Edge *e1 = new Edge(v, 1), *e2 = new Edge(u + 1, 0);
+    e1->dual = e2;
+    e2->dual = e1;
+    adj[u + 1].pb(e1);
+    adj[v].pb(e2);
+    e1 = new Edge(u, 1), e2 = new Edge(v + 1, 0);
+    e1->dual = e2;
+    e2->dual = e1;
+    adj[v + 1].pb(e1);
+    adj[u].pb(e2);
+  }
+
+  int ans = 0;
+  while (1) {
+    vector<int> prev(MX, -1);
+    Edge* path[MX]{};
+    queue<int> q;
+    q.push(S);
+    while (!q.empty() && prev[T] == -1) {
+      int cur = q.front();
+      q.pop();
+      for (auto& e : adj[cur]) {
+        int nxt = e->to;
+        if (e->spare() && prev[nxt] == -1) {
+          q.push(nxt);
+          prev[nxt] = cur;
+          path[nxt] = e;
+          if (prev[T] != -1) break;
+        }
+      }
+    }
+
+    if (prev[T] == -1) break;
+
+    int flow = INF;
+    for (int i = T; i != S; i = prev[i]) flow = min(flow, path[i]->spare());
+    for (int i = T; i != S; i = prev[i]) path[i]->addFlow(flow);
+    ans += flow;
+  }
+
+  cout << ans << "\n";
+}
