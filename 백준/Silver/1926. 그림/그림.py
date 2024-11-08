6546926121269773b32625f96b65b50eba54d81a
kushinada <111179843+kushinada2077@users.noreply.github.com>
2024-11-08 20:05:10 +0900

    [Silver I] Title: 그림, Time: 224 ms, Memory: 119544 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.py
+++ 백준/Silver/1926. 그림/그림.py
@@ -1,36 +1,43 @@
+import sys
 from collections import deque
 
 dx = [1, 0, -1, 0]
 dy = [0, 1, 0, -1]
-vis = [[0 for i in range(500)] for j in range(500)]
 
-def bfs(sx, sy):
+vis = [[0 for _ in range(505)] for _ in range(505)]    
+
+def bfs(sy, sx):
+  q = deque()
   ret = 0
-  dq = deque()
-  dq.append([sx, sy])
-  vis[sx][sy] = 1 # 방문 체크
-   
-  while len(dq):
-    x, y = dq.popleft() # 큐에서 뽑은 현재 좌표
-    ret += 1
+  
+  q.append((sy, sx))
+  vis[sy][sx] = 1
+
+  while len(q):
+    y, x = q.popleft()       
+    ret += 1  
+
     for dir in range(4):
-      nx = x + dx[dir] # 다음 x좌표
-      ny = y + dy[dir] # 다음 y좌표
-      if nx < 0 or nx >= n or ny < 0 or ny >= m: continue
-      if paper[nx][ny] == 0: continue
-      if vis[nx][ny]: continue
-      dq.append([nx, ny])
-      vis[nx][ny] = 1
+      ny = y + dy[dir] 
+      nx = x + dx[dir]
+      # 내가 보고 있는 정점이 방문을 했다면
+      if ny < 0 or ny >= n or nx < 0 or nx >= m: continue
+      if vis[ny][nx] == 1 or board[ny][nx] == 0: continue
+      q.append((ny, nx)) 
+      vis[ny][nx] = 1
+  
   return ret
 
 n, m = map(int, input().split())
-paper = [list(map(int, input().split())) for i in range(n)]
-ans1 = 0
+board = [list(map(int, input().split())) for _ in range(n)]
+
+ans = 0
 ans2 = 0
 for i in range(n):
   for j in range(m):
-    if paper[i][j] == 1 and vis[i][j] == 0:
-      ans1 += 1
-      ans2 = max(ans2, bfs(i, j))
+    if (vis[i][j] or board[i][j] == 0): continue
+    ans += 1
+    ans2 = max(ans2, bfs(i, j))
 
-print(ans1, ans2, sep='\n')
+print(ans)
+print(ans2)    
