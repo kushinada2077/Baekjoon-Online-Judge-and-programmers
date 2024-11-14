58246f595ffc2a28d5f7516469627671b99d854c
kushinada <111179843+kushinada2077@users.noreply.github.com>
2024-11-14 17:36:07 +0900

    [Gold IV] Title: 고층 건물, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/1027. 고층 건물/고층 건물.cc
+++ 백준/Gold/1027. 고층 건물/고층 건물.cc
@@ -4,7 +4,6 @@
 #include <map>
 #include <queue>
 #include <set>
-#include <sstream>
 #include <vector>
 #define PATH "/Users/leedongha/Downloads/PS/input.txt"
 #define L_PATH "input.txt"
@@ -22,66 +21,35 @@ using ll = long long;
 using TP = tuple<int, int, int>;
 using P = pair<int, int>;
 
-inline ll cross(ll x1, ll y1, ll x2, ll y2) { return x1 * y2 - x2 * y1; }
-
-ll n, arr[51];
+int n, arr[51];
 int main() {
   fastio;
   cin >> n;
-  for (int i = 0; i < n; ++i) cin >> arr[i];
   int ans = 0;
+  for (int i = 0; i < n; ++i) cin >> arr[i];
 
-  for (ll ax, ay, bx, by, i = 0; i < n; ++i) {
-    int cnt = 0, cntt = 0;
-    for (ll j = i - 1; j >= 0; --j) {
-      bool f = 1;
-      ax = i;
-      ay = arr[i];
-      bx = j;
-      by = arr[j];
-
-      for (ll cx, cy, dx, dy, k = i - 1; k > j; --k) {
-        cx = dx = k;
-        cy = 0;
-        dy = arr[k];
-        ll d1 = cross(bx - ax, by - ay, cx - ax, cy - ay), d2 = cross(bx - ax, by - ay, dx - ax, dy - ay);
-        if (!(d1 > 0 && d2 > 0 || d1 < 0 && d2 < 0 || d1 == 0 && d2 == 0)) {
-          f = 0;
-          break;
-        }
+  for (int i = 0; i < n; ++i) {
+    double mx = -INF;
+    int cnt{};
+    for (int j = i + 1; j < n; ++j) {
+      double d = ((double)arr[j] - arr[i]) / (j - i);
+      if (mx < d) {
+        mx = d;
+        cnt++;
       }
-
-      if (!f) continue;
-      cnt++;
     }
 
-    cntt += cnt;
-    cnt = 0;
+    double mn = INF;
 
-    for (ll j = i + 1; j < n; ++j) {
-      bool f = 1;
-      ax = i;
-      ay = arr[i];
-      bx = j;
-      by = arr[j];
-
-      for (ll cx, cy, dx, dy, k = i + 1; k < j; ++k) {
-        cx = dx = k;
-        cy = 0;
-        dy = arr[k];
-        ll d1 = cross(bx - ax, by - ay, cx - ax, cy - ay), d2 = cross(bx - ax, by - ay, dx - ax, dy - ay);
-        if (!(d1 > 0 && d2 > 0 || d1 < 0 && d2 < 0 || d1 == 0 && d2 == 0)) {
-          f = 0;
-          break;
-        }
+    for (int j = i - 1; j >= 0; --j) {
+      double d = ((double)arr[j] - arr[i]) / (j - i);
+      if (mn > d) {
+        mn = d;
+        cnt++;
       }
-
-      if (!f) continue;
-      cnt++;
     }
 
-    cntt += cnt;
-    ans = max(ans, cntt);
+    ans = max(ans, cnt);
   }
 
   cout << ans << "\n";
