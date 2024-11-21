4b2fa3601192abe009130ec13e8dac77a7c091b6
kushinada <111179843+kushinada2077@users.noreply.github.com>
2024-11-21 12:13:29 +0900

    [Gold V] Title: 선 긋기, Time: 360 ms, Memory: 14440 KB -BaekjoonHub

--- 백준/Gold/2170. 선 긋기/선 긋기.cc
+++ 백준/Gold/2170. 선 긋기/선 긋기.cc
@@ -1,46 +1,54 @@
 #include <algorithm>
-#include <climits>
-#include <deque>
+#include <cmath>
 #include <iostream>
 #include <map>
-#include <numeric>
 #include <queue>
 #include <set>
-#include <stack>
-#include <tuple>
 #include <vector>
+#define PATH "/Users/leedongha/Downloads/PS/input.txt"
+#define L_PATH "input.txt"
 #define fastio cin.tie(0)->sync_with_stdio(0);
-#define for_in(n) for (int i = 0; i < n; ++i)
+#define rep(n) for (int i = 0; i < n; ++i)
 #define si(x) int(x.size())
 #define all(x) (x).begin(), (x).end()
 #define pb(...) push_back(__VA_ARGS__)
 #define X first
 #define Y second
 #define ROOT 1
-using ll = long long;
+#define INF 0x3f3f3f3f
 using namespace std;
+using ll = long long;
+using TP = tuple<int, int, int>;
+using P = pair<int, int>;
 
+int n;
+vector<P> llist;
 int main() {
   fastio;
-  int n, x, y;
   cin >> n;
-  vector<pair<int, int>> a;
-  for (int i = 0; i < n; ++i) {
-    cin >> x >> y;
-    a.pb({x, y});
+  for (int l, r, i{}; i < n; ++i) {
+    cin >> l >> r;
+    llist.pb(P(l, -r));
   }
-  sort(all(a));
-  ll ans = 0;
-  int l, r;
-  tie(l, r) = a[0];
+
+  sort(all(llist));
+
+  auto [curL, curR] = llist[0];
+  curR *= -1;
+  ll ans = curR - curL;
+
   for (int i = 1; i < n; ++i) {
-    auto [nl, nr] = a[i];
-    if (r < nl) {
-      ans += r - l;
-      l = nl;
-      r = nr;
-    } else r = max(r, nr);
+    auto [l, r] = llist[i];
+    r *= -1;
+
+    if (curR < l) {
+      curL = l;
+      curR = r;
+      ans += 1ll * r - l;
+    } else if (curR < r) {
+      ans += 1ll * r - curR;
+      curR = r;
+    }
   }
-  ans += r - l;
   cout << ans << "\n";
 }
