b4039ae5988f3bb0bc91a44edd22606592a7669b
kushinada <111179843+kushinada2077@users.noreply.github.com>
2024-11-29 20:30:27 +0900

    [Silver II] Title: N과 M (9), Time: 12 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/15663. N과 M （9）/N과 M （9）.cc
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.cc
@@ -1,52 +1,49 @@
 #include <algorithm>
 #include <cmath>
-#include <deque>
 #include <iostream>
+#include <map>
 #include <queue>
-#include <tuple>
-#include <unordered_set>
+#include <set>
 #include <vector>
+#define PATH "/Users/leedongha/Downloads/PS/input.txt"
+#define L_PATH "input.txt"
 #define fastio cin.tie(0)->sync_with_stdio(0);
-#define for_in(n) for (int i = 0; i < n; ++i)
+#define rep(n) for (int i = 0; i < n; ++i)
 #define si(x) int(x.size())
 #define all(x) (x).begin(), (x).end()
 #define pb(...) push_back(__VA_ARGS__)
 #define X first
 #define Y second
-using ll = long long;
+#define ROOT 1
+#define INF 0x3f3f3f3f
 using namespace std;
+using ll = long long;
+using TP = tuple<int, int, int>;
+using P = pair<int, int>;
 
-int n, m, x;
-vector<int> a, num;
-vector<bool> used(10, false);
-unordered_set<string> se;
-void f(int v) {
-  if (v == m) {
-    string tmp = "";
-    for (auto& i : num) tmp += i;
-    if (se.find(tmp) != se.end()) return;
-    se.insert(tmp);
-    for (auto& c : num) cout << c << " ";
+int n, m, seq[10], arr[10];
+bool isUsed[10];
+void sol(int k) {
+  if (k == m) {
+    for (int i = 0; i < m; ++i) cout << seq[i] << " ";
     cout << "\n";
+    return;
   }
 
+  int pre{};
   for (int i = 0; i < n; ++i) {
-    if (!used[i]) {
-      used[i] = true;
-      num.pb(a[i]);
-      f(v + 1);
-      used[i] = false;
-      num.pop_back();
-    }
+    if (isUsed[i] || pre == arr[i]) continue;
+    isUsed[i] = 1;
+    pre = seq[k] = arr[i];
+    sol(k + 1);
+    isUsed[i] = 0;
   }
 }
+
 int main() {
   fastio;
   cin >> n >> m;
-  for (int i = 0; i < n; ++i) {
-    cin >> x;
-    a.pb(x);
-  }
-  sort(all(a));
-  f(0);
+  for (int i = 0; i < n; ++i) cin >> arr[i];
+  sort(arr, arr + n);
+  sol(0);
 }
