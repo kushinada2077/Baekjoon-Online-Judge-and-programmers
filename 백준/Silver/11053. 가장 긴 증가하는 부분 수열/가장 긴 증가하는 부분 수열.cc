ee8d1ab6cecf47eee95482b04a78068269e85b53
kushinada <111179843+kushinada2077@users.noreply.github.com>
2024-12-02 13:59:06 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.cc
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.cc
@@ -1,26 +1,43 @@
-#define ll long long
-#define PAIR pair<int, int>
 #include <algorithm>
+#include <cmath>
 #include <iostream>
+#include <map>
 #include <queue>
 #include <set>
 #include <vector>
+#define PATH "/Users/leedongha/Downloads/PS/input.txt"
+#define L_PATH "input.txt"
+#define fastio cin.tie(0)->sync_with_stdio(0);
+#define rep(n) for (int i = 0; i < n; ++i)
+#define si(x) int(x.size())
+#define all(x) (x).begin(), (x).end()
+#define pb(...) push_back(__VA_ARGS__)
+#define X first
+#define Y second
+#define ROOT 1
+#define INF 0x3f3f3f3f
 using namespace std;
+using ll = long long;
+using TP = tuple<int, int, int>;
+using P = pair<int, int>;
 
-int n, v, a[1005], d[1005];
+// lis(start) = A[start]에서 시작하는 부분 증가 수열 중 최대 길이
 
+int n, A[1001], dp[1002];
+int lis(int start) {
+  int& ret = dp[start + 1];
+  if (ret != -1) return ret;
+  ret = 1;
+  for (int next = start + 1; next < n; ++next) {
+    if (A[start] < A[next]) ret = max(ret, lis(next) + 1);
+  }
+
+  return ret;
+}
 int main() {
-  ios::sync_with_stdio(0);
-  cin.tie(0);
+  fastio;
   cin >> n;
-  for (int i = 0; i < n; ++i) {
-    cin >> a[i];
-    d[i] = 1;
-  }
-  for (int i = 0; i < n; ++i) {
-    for (int j = 0; j < i; ++j) {
-      if (a[i] > a[j]) d[i] = max(d[j] + 1, d[i]);
-    }
-  }
-  cout << *max_element(d, d + n);
+  for (int i = 0; i < n; ++i) cin >> A[i];
+  fill(dp, dp + 1002, -1);
+  cout << lis(-1) - 1 << "\n";
 }
