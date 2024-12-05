ea522f176f51132f84cc1baf3b42f814b679e22f
kushinada <111179843+kushinada2077@users.noreply.github.com>
2024-12-05 16:48:19 +0900

    [Silver I] Title: 곱셈, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1629. 곱셈/곱셈.cc
+++ 백준/Silver/1629. 곱셈/곱셈.cc
@@ -1,27 +1,35 @@
-#define PAIR pair<long long, int>
-#define ll long long
 #include <algorithm>
+#include <cmath>
 #include <iostream>
 #include <map>
+#include <queue>
+#include <set>
 #include <vector>
+#define PATH "/Users/leedongha/Downloads/PS/input.txt"
+#define L_PATH "input.txt"
+#define fastio cin.tie(0)->sync_with_stdio(0);
+#define rep(n) for (int i = 0; i < n; ++i)
+#define si(x) int(x.size())
+#define all(x) (x).begin(), (x).end()
+#define pb(...) push_back(__VA_ARGS__)
+#define X first
+#define Y second
+#define ROOT 1
+#define INF 0x3f3f3f3f
 using namespace std;
+using ll = long long;
+using TP = tuple<int, int, int>;
+using P = pair<int, int>;
 
-ll mod;
-ll pow(ll a, ll b) {
-  ll ret = 1;
-  while (b) {
-    if (b % 2) ret = ret * a % mod;
-    a = a * a % mod;
-    b /= 2;
-  }
-
-  return ret;
+int a, b, c;
+int my_pow(int n, int r) {
+  if (r == 0) return 1;
+  if (r % 2) return (1ll * n * my_pow(n, r - 1)) % c;
+  int half = my_pow(n, r / 2);
+  return (1ll * half * half) % c;
 }
-
 int main() {
-  ios::sync_with_stdio(0);
-  cin.tie(0);
-  ll a, b;
-  cin >> a >> b >> mod;
-  cout << pow(a, b);
+  fastio;
+  cin >> a >> b >> c;
+  cout << my_pow(a, b) << "\n";
 }
