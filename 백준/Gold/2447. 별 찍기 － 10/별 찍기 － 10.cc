02634e713b6bf2eb6cdb2dc8428528b314ebf3ad
kushinada <111179843+kushinada2077@users.noreply.github.com>
2024-12-06 20:40:20 +0900

    [Gold V] Title: 별 찍기 - 10, Time: 96 ms, Memory: 6748 KB -BaekjoonHub

--- 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.cc
+++ 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.cc
@@ -24,27 +24,29 @@ using P = pair<int, int>;
 int n;
 char board[2200][2200];
 // (y, x)부터 sz 크기 별 찍기
-void solve(int y, int x, int sz, int type) {
+void solve(int y, int x, int sz) {
   // 기저 사례: sz가 1이면 찍음
   if (sz == 1) {
-    if (type) board[y][x] = '*';
-    else board[y][x] = ' ';
+    board[y][x] = '*';
     return;
   }
 
   for (int i = 0; i < 3; ++i) {
     for (int j = 0; j < 3; ++j) {
-      if (3 * i + j == 4) solve(y + i * sz / 3, x + j * sz / 3, sz / 3, 0);
-      else solve(y + i * sz / 3, x + j * sz / 3, sz / 3, type);
+      if (3 * i + j == 4) continue;
+      solve(y + i * sz / 3, x + j * sz / 3, sz / 3);
     }
   }
 }
 int main() {
   fastio;
   cin >> n;
-  solve(0, 0, n, 1);
+  solve(0, 0, n);
   for (int i = 0; i < n; ++i) {
-    for (int j = 0; j < n; ++j) cout << board[i][j];
+    for (int j = 0; j < n; ++j) {
+      if (board[i][j] == 0) cout << " ";
+      else cout << board[i][j];
+    }
     cout << "\n";
   }
 }
