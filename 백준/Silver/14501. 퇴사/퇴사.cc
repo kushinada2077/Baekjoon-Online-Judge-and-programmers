05e673f16ed4e862970b6224931775569d2d6544
kushinada <111179843+kushinada2077@users.noreply.github.com>
2024-12-21 20:21:59 +0900

    [Silver III] Title: 퇴사, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.cc
+++ 백준/Silver/14501. 퇴사/퇴사.cc
@@ -1,25 +1,46 @@
-#define ll long long
-#define PAIR pair<int, int>
 #include <algorithm>
+#include <cmath>
 #include <iostream>
-#include <iterator>
+#include <map>
 #include <queue>
+#include <set>
 #include <vector>
+#define PATH "/Users/leedongha/Downloads/PS/input.txt"
+#define L_PATH "input.txt"
+#define fastio cin.tie(0)->sync_with_stdio(0);
+#define rep(n) for (int i = 0; i < n; ++i)
+#define si(x) int(x.size())
+#define all(x) (x).begin(), (x).end()
+#define pb(...) push_back(__VA_ARGS__)
+#define X first
+#define Y second
+#define ROOT 1
+#define INF 0x3f3f3f3f
 using namespace std;
+using ll = long long;
+using TP = tuple<int, int, int>;
+using P = pair<int, int>;
 
-int n, t[16], p[16], d[16];
+// dp[x] = x일부터 상담을 시작했을 때 얻을 수 있는 최대 수익
+int n, dp[20], t[20], p[20];
+
+int maxProfit(int x) {
+  if (x == n) return 0;
+  if (x > n) return -INF;
+  int& ret = dp[x];
+  if (ret != -1) return ret;
+  ret = maxProfit(x + 1);
+  if (x + t[x] <= n) ret = max(ret, p[x] + maxProfit(x + t[x]));
+  return ret;
+}
+
 int main() {
-  ios::sync_with_stdio(0);
-  cin.tie(0);
+  fastio;
   cin >> n;
-  for (int i = 0; i < n; ++i) cin >> t[i] >> p[i];
-
-  for (int i = n - 1; i >= 0; --i) {
-    int nxt = i + t[i];
-    if (nxt <= n)
-      d[i] = max(d[nxt] + p[i], d[i + 1]);
-    else
-      d[i] = d[i + 1];
+  fill(dp, dp + 20, -1);
+  for (int i = 0; i < n; ++i) {
+    cin >> t[i] >> p[i];
   }
-  cout << *max_element(d, d + n);
+
+  cout << maxProfit(0) << "\n";
 }
