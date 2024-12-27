58af3bcaf17df2d13afb66f2514188390a0d7ef2
kushinada <111179843+kushinada2077@users.noreply.github.com>
2024-12-27 20:06:06 +0900

    [Silver IV] Title: 순서쌍의 곱의 합, Time: 8 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Silver/13900. 순서쌍의 곱의 합/순서쌍의 곱의 합.cc
+++ 백준/Silver/13900. 순서쌍의 곱의 합/순서쌍의 곱의 합.cc
@@ -8,18 +8,15 @@ using ll = long long;
 using TP = tuple<int, int, int>;
 using P = pair<int, int>;
 
-int n, A[100005], pref[100005];
+int n, A[100005];
+ll sum, ans;
 int main() {
   fastio;
   cin >> n;
-  for (int i = 0; i < n; ++i) {
-    cin >> A[i];
-    pref[i + 1] = pref[i] + A[i];
-  }
-
-  ll ans = 0;
-  for (int i = 0; i < n; ++i) {
-    ans += 1ll * A[i] * (pref[n] - pref[i + 1]);
+  for (int i = 0; i < n; ++i) cin >> A[i];
+  for (int i = n - 1; i != 0; --i) {
+    sum += A[i];
+    ans += sum * A[i - 1];
   }
 
   cout << ans << "\n";
