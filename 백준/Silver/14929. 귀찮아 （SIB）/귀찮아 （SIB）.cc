606c5bc99cab765f17cc233d95cdad71a4749ca1
kushinada <111179843+kushinada2077@users.noreply.github.com>
2024-12-27 20:00:46 +0900

    [Silver V] Title: 귀찮아 (SIB), Time: 8 ms, Memory: 2800 KB -BaekjoonHub

---
+++ 백준/Silver/14929. 귀찮아 （SIB）/귀찮아 （SIB）.cc
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+#define fastio cin.tie(0)->sync_with_stdio(0);
+#define si(x) int(x.size())
+#define all(x) (x).begin(), (x).end()
+#define INF 0x3f3f3f3f
+using namespace std;
+using ll = long long;
+using TP = tuple<int, int, int>;
+using P = pair<int, int>;
+
+int n, A[100005], pref[100005];
+int main() {
+  fastio;
+  cin >> n;
+  for (int i = 0; i < n; ++i) {
+    cin >> A[i];
+    pref[i + 1] = pref[i] + A[i];
+  }
+
+  ll ans = 0;
+  for (int i = 0; i < n; ++i) {
+    ans += A[i] * (pref[n] - pref[i + 1]);
+  }
+
+  cout << ans << "\n";
+}
