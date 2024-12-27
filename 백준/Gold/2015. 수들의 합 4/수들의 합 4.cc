9c968dfd2e17073ec602859558486cb0cd11f107
kushinada <111179843+kushinada2077@users.noreply.github.com>
2024-12-28 07:27:47 +0900

    [Gold IV] Title: 수들의 합 4, Time: 40 ms, Memory: 12848 KB -BaekjoonHub

--- 백준/Gold/2015. 수들의 합 4/수들의 합 4.cc
+++ 백준/Gold/2015. 수들의 합 4/수들의 합 4.cc
@@ -8,13 +8,13 @@ using ll = long long;
 using TP = tuple<int, int, int>;
 using P = pair<int, int>;
 
-ll n, k, ans, sum, x;
+ll n, k, ans, sum;
 map<ll, int> cnt;
 int main() {
   fastio;
   cin >> n >> k;
   cnt[0]++;
-  for (int i = 0; i < n; ++i) {
+  for (int x, i = 0; i < n; ++i) {
     cin >> x;
     sum += x;
     ans += cnt[sum - k];
