1141fed18b2e75eed4a302b997c1cc5f5ba61e92
kushinada <111179843+kushinada2077@users.noreply.github.com>
2024-12-29 14:16:08 +0900

    [Gold IV] Title: 휴게소 세우기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/1477. 휴게소 세우기/휴게소 세우기.cc
+++ 백준/Gold/1477. 휴게소 세우기/휴게소 세우기.cc
@@ -1,41 +1,27 @@
-#include <algorithm>
-#include <iostream>
-#include <stack>
-#include <vector>
-#define ll long long
-#define fastio cin.tie(0)->sync_with_stdio(0);
-#define FOR_IN_1(n) for (int i = 0; i < n; ++i)
-#define FOR_IN_2(i, n) for (int i = 0; i < n; ++i)
-#define FOR_IN_3(i, m, n) for (int i = m; i < n; ++i)
-#define GET_MACRO(_1, _2, _3, NAME, ...) NAME
-#define for_in(...) GET_MACRO(__VA_ARGS__, FOR_IN_3, FOR_IN_2, FOR_IN_1)(__VA_ARGS__)
-#define si(x) int(x.size())
-#define all(x) (x).begin(), (x).end()
-#define pb(x) push_back(x)
+#include <bits/stdc++.h>
 using namespace std;
+using i64 = long long;
+using TPi = tuple<int, int, int>;
+using Pi = pair<int, int>;
 
-int round_up(int a, int b) { return (a + b - 1) / b; }
-int max_distance(vector<int>& dis, vector<int>& div) {
-  int idx = 0;
-  for_in(si(dis)) if (round_up(dis[i], div[i]) > round_up(dis[idx], div[idx])) idx = i;
-  return idx;
-}
+int n, m, l;
+priority_queue<TPi> pq;
 int main() {
-  fastio;
-  int n, m, l;
+  cin.tie(nullptr)->sync_with_stdio(false);
   cin >> n >> m >> l;
   vector<int> a(n + 1);
-  a[0] = 0;
-  for_in(i, 1, n + 1) cin >> a[i];
-  a.pb(l);
-  sort(all(a));
-  vector<int> dis;
-  for_in(i, 1, n + 2) dis.pb(a[i] - a[i - 1]);
-  vector<int> div(si(dis), 1);
-  while (m--) {
-    int idx = max_distance(dis, div);
-    div[idx]++;
+  for (int i = 1; i <= n; ++i) cin >> a[i];
+  a.push_back(l);
+  sort(a.begin(), a.end());
+  for (int i = 0; i < n + 1; ++i) {
+    pq.push(TPi(a[i + 1] - a[i], a[i + 1] - a[i], 1));
   }
-  int ans_idx = max_distance(dis, div);
-  cout << round_up(dis[ans_idx], div[ans_idx]);
+  while (m--) {
+    auto [curDiff, oriDiff, numDiv] = pq.top();
+    pq.pop();
+    numDiv++;
+    pq.push(TPi((oriDiff + numDiv - 1) / numDiv, oriDiff, numDiv));
+  }
+
+  cout << get<0>(pq.top()) << "\n";
 }
