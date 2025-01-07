3cec27aa3c6b56b54069b65faf759faee65dc7ba
kushinada <111179843+kushinada2077@users.noreply.github.com>
2025-01-07 19:00:30 +0900

    [Silver V] Title: 아무래도이문제는A번난이도인것같다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1402. 아무래도이문제는A번난이도인것같다/아무래도이문제는A번난이도인것같다.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using i64 = long long;
+
+int main() {
+  std::cin.tie(nullptr)->sync_with_stdio(false);
+  int t;
+  std::cin >> t;
+  while (t--) {
+    int a, b;
+    std::cin >> a >> b;
+    std::cout << "yes\n";
+  }
+}
