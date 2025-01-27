cffaf28d89f04572b3c18b1a7213deebf6531526
kushinada <111179843+kushinada2077@users.noreply.github.com>
2025-01-27 14:17:49 +0900

    [Gold V] Title: 동전 2, Time: 0 ms, Memory: 2180 KB -BaekjoonHub

--- 백준/Gold/2294. 동전 2/동전 2.cc
+++ 백준/Gold/2294. 동전 2/동전 2.cc
@@ -1,42 +1,20 @@
-#include <algorithm>
-#include <climits>
-#include <deque>
-#include <iostream>
-#include <map>
-#include <numeric>
-#include <queue>
-#include <set>
-#include <stack>
-#include <tuple>
-#include <unordered_map>
-#include <unordered_set>
-#include <vector>
-#define PATH "/Users/leedongha/Downloads/PS/input.txt"
-#define fastio cin.tie(0)->sync_with_stdio(0);
-#define for_in(n) for (int i = 0; i < n; ++i)
-#define si(x) int(x.size())
-#define all(x) (x).begin(), (x).end()
-#define pb(...) push_back(__VA_ARGS__)
-#define X first
-#define Y second
-#define ROOT 1
-#define INF 0x3f3f3f3f
-using ll = long long;
-using namespace std;
+#include <bits/stdc++.h>
+using i64 = long long;
 
-int dp[10005];
-int n, k;
 int main() {
-  fastio;
-  cin >> n >> k;
-  vector<int> a(n + 1);
-  for (int i = 1; i <= n; ++i) cin >> a[i];
-  for (int i = 1; i <= k; ++i) {
-    dp[i] = INF;
-    for (int j = 1; j <= n; ++j) {
-      if (i - a[j] >= 0) dp[i] = min(dp[i], dp[i - a[j]] + 1);
+  std::cin.tie(nullptr)->sync_with_stdio(false);
+  int n, k;
+  std::cin >> n >> k;
+  std::vector<int> dp(k + 1, 0x3f3f3f3f);
+  dp[0] = 0;
+  for (int i = 0; i < n; ++i) {
+    int v;
+    std::cin >> v;
+    for (int j = 1; j <= k; ++j) {
+      if (j - v >= 0) dp[j] = std::min(dp[j], dp[j - v] + 1);
     }
   }
-  if (dp[k] == INF) cout << "-1\n";
-  else cout << dp[k] << "\n";
+
+  if (dp[k] == 0x3f3f3f3f) dp[k] = -1;
+  std::cout << dp[k] << "\n";
 }
