dae5edc38f41957999e3f29edde5e0a5ad044502
kushinada <111179843+kushinada2077@users.noreply.github.com>
2025-01-27 14:06:24 +0900

    [Gold V] Title: 평범한 배낭, Time: 12 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
@@ -1,37 +1,19 @@
-#include <algorithm>
-#include <climits>
-#include <iostream>
-#include <stack>
-#include <vector>
-#define PATH "/Users/leedongha/Downloads/PS/input.txt"
-#define fastio cin.tie(0)->sync_with_stdio(0);
-#define for_in(n) for (int i = 0; i < n; ++i)
-#define si(x) int(x.size())
-#define all(x) (x).begin(), (x).end()
-#define pb(...) push_back(__VA_ARGS__)
-#define X first
-#define Y second
-#define ROOT 1
-#define INF 0x3f3f3f3f
-using ll = long long;
-using namespace std;
+#include <bits/stdc++.h>
+using i64 = long long;
 
-int dp[105][100005];
 int main() {
-  fastio;
-  int n, k, w, v;
-  cin >> n >> k;
-  vector<pair<int, int>> a(n + 1);
-  for (int i = 1; i <= n; ++i) {
-    cin >> a[i].X >> a[i].Y;
-  }
-
-  for (int i = 1; i <= n; ++i) {
-    for (int j = 1; j <= k; ++j) {
-      dp[i][j] = dp[i - 1][j];
-      if (j - a[i].X >= 0) dp[i][j] = max(dp[i][j], dp[i - 1][j - a[i].X] + a[i].Y);
+  std::cin.tie(nullptr)->sync_with_stdio(false);
+  int n, k;
+  std::cin >> n >> k;
+  std::vector<int> dp(k + 1, -1);
+  dp[0] = 0;
+  for (int i = 0; i < n; ++i) {
+    int w, v;
+    std::cin >> w >> v;
+    for (int j = k; j >= 1; --j) {
+      if (j - w >= 0 && dp[j - w] != -1) dp[j] = std::max(dp[j], dp[j - w] + v);
     }
   }
 
-  cout << dp[n][k] << "\n";
+  std::cout << *max_element(dp.begin(), dp.end()) << "\n";
 }
