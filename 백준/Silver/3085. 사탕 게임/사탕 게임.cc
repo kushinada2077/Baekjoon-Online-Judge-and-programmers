bdb46eaa5dcca0880e5d9bc28c7cab7095149a35
kushinada <111179843+kushinada2077@users.noreply.github.com>
2025-02-26 10:18:21 +0900

    [Silver II] Title: 사탕 게임, Time: 12 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/3085. 사탕 게임/사탕 게임.cc
+++ 백준/Silver/3085. 사탕 게임/사탕 게임.cc
@@ -1,65 +1,64 @@
-#include <algorithm>
-#include <iostream>
-#include <queue>
-#include <stack>
-#include <vector>
-#define PATH "/Users/leedongha/Downloads/PS/input.txt"
-#define fastio cin.tie(0)->sync_with_stdio(0);
-#define for_in(n) for (int i = 0; i < n; ++i)
-#define si(x) int(x.size())
-#define all(x) (x).begin(), (x).end()
-#define pb(...) push_back(__VA_ARGS__)
-#define X first
-#define Y second
-#define ROOT 1
-#define INF 0x3f3f3f3f
-using ll = long long;
-using namespace std;
-
-string board[51];
+#include <bits/stdc++.h>
+using i64 = long long;
 
 int main() {
-  fastio;
-  int n, ans = 1;
-  cin >> n;
-  for (int i = 0; i < n; ++i) {
-    cin >> board[i];
-  }
-
+  std::cin.tie(nullptr)->sync_with_stdio(false);
+  int n;
+  std::cin >> n;
+  std::vector a(n, std::vector<char>(n));
   for (int i = 0; i < n; ++i) {
     for (int j = 0; j < n; ++j) {
-      for (int dir = 0; dir < 4; ++dir) {
-        int x = i + "2101"[dir] - '1', y = j + "1210"[dir] - '1';
-        if (x < 0 || x >= n || y < 0 || y >= n) continue;
-        swap(board[i][j], board[x][y]);
-        for (int r = 0; r < n; ++r) {
-          int cnt = 1, cur = board[r][0];
-          for (int c = 1; c < n; ++c) {
-            if (cur != board[r][c]) {
-              ans = max(ans, cnt);
-              cnt = 0;
-              cur = board[r][c];
-            }
-            cnt++;
-          }
-          ans = max(ans, cnt);
+      std::cin >> a[i][j];
+    }
+  }
+
+  auto max_seq = [&]() {
+    int ret = 1;
+    for (int i = 0; i < n; ++i) {
+      int cnt = 1;
+      for (int j = 1; j < n; ++j) {
+        if (a[i][j] != a[i][j - 1]) {
+          cnt = 1;
+        } else {
+          cnt++;
         }
-        for (int c = 0; c < n; ++c) {
-          int cnt = 1, cur = board[0][c];
-          for (int r = 1; r < n; ++r) {
-            if (cur != board[r][c]) {
-              ans = max(ans, cnt);
-              cnt = 0;
-              cur = board[r][c];
-            }
-            cnt++;
-          }
-          ans = max(ans, cnt);
+        ret = std::max(ret, cnt);
+      }
+    }
+
+    for (int j = 0; j < n; ++j) {
+      int cnt = 1;
+      for (int i = 1; i < n; ++i) {
+        if (a[i][j] != a[i - 1][j]) {
+          cnt = 1;
+        } else {
+          cnt++;
         }
-        swap(board[i][j], board[x][y]);
+        ret = std::max(ret, cnt);
+      }
+    }
+
+    return ret;
+  };
+
+  int ans = 0;
+  for (int i = 0; i < n; ++i) {
+    for (int j = 0; j < n; ++j) {
+      for (int dir = 0; dir < 2; ++dir) {
+        int ni = i + "21"[dir] - '1';
+        int nj = j + "12"[dir] - '1';
+        if (ni < 0 || ni >= n || nj < 0 || nj >= n) {
+          continue;
+        }
+        if (a[i][j] == a[ni][nj]) {
+          continue;
+        }
+        std::swap(a[i][j], a[ni][nj]);
+        ans = std::max(ans, max_seq());
+        std::swap(a[i][j], a[ni][nj]);
       }
     }
   }
 
-  cout << ans << "\n";
+  std::cout << ans << "\n";
 }
