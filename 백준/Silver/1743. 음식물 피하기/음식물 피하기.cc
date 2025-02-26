0a9e13227a4e71b71ae41ebacd1250ef12315ae8
kushinada <111179843+kushinada2077@users.noreply.github.com>
2025-02-26 10:44:15 +0900

    [Silver I] Title: 음식물 피하기, Time: 0 ms, Memory: 2156 KB -BaekjoonHub

--- 백준/Silver/1743. 음식물 피하기/음식물 피하기.cc
+++ 백준/Silver/1743. 음식물 피하기/음식물 피하기.cc
@@ -1,50 +1,51 @@
-#include <algorithm>
-#include <climits>
-#include <iostream>
-#include <stack>
-#include <vector>
-#define PATH "/Users/leedongha/Downloads/PS/input.txt"
-#define fastio cin.tie(0)->sync_with_stdio(0);
-#define for_in(n) for (int i = 0; i < n; ++i)
-#define si(x) int(x.size())
-#define all(x) (x).begin(), (x).end()
-#define pb(...) push_back(__VA_ARGS__)
-#define X first
-#define Y second
-#define ROOT 1
-#define INF 0x3f3f3f3f
-using ll = long long;
-using namespace std;
+#include <bits/stdc++.h>
+using i64 = long long;
 
-int n, m, k;
-vector<vector<bool>> vis(105, vector<bool>(105)), board(105, vector<bool>(105));
-int dfs(int y, int x) {
-  int ret = 1;
-  for (int dir = 0; dir < 4; ++dir) {
-    int ny = y + "2101"[dir] - '1', nx = x + "1210"[dir] - '1';
-    if (ny <= 0 || ny > n || nx <= 0 || nx > m) continue;
-    if (vis[ny][nx] || board[ny][nx] == 0) continue;
-    vis[ny][nx] = true;
-    ret += dfs(ny, nx);
-  }
-  return ret;
-}
 int main() {
-  fastio;
-  int x, y;
-  vector<pair<int, int>> coor;
-  cin >> n >> m >> k;
-  while (k--) {
-    cin >> y >> x;
-    board[y][x] = 1;
-    coor.pb({y, x});
+  std::cin.tie(nullptr)->sync_with_stdio(false);
+  int n, m, k;
+  std::cin >> n >> m >> k;
+  std::vector a(n, std::vector<int>(m));
+  std::vector vis(n, std::vector<bool>(m));
+
+  for (int i = 0; i < k; ++i) {
+    int y, x;
+    std::cin >> y >> x;
+    a[y - 1][x - 1] = 1;
   }
 
-  int ans = -1;
-  for (auto [y, x] : coor) {
-    if (vis[y][x]) continue;
-    vis[y][x] = 1;
-    ans = max(ans, dfs(y, x));
+  int ans = 0;
+  for (int i = 0; i < n; ++i) {
+    for (int j = 0; j < m; ++j) {
+      if (a[i][j] == 0 || vis[i][j] == true) {
+        continue;
+      }
+
+      std::queue<std::pair<int, int>> q;
+      q.push({i, j});
+      vis[i][j] = true;
+      int cnt = 0;
+      while (!q.empty()) {
+        auto [y, x] = q.front();
+        q.pop();
+        cnt++;
+        for (int dir = 0; dir < 4; ++dir) {
+          int ny = y + "2101"[dir] - '1';
+          int nx = x + "1210"[dir] - '1';
+          if (ny < 0 || ny >= n || nx < 0 || nx >= m) {
+            continue;
+          }
+          if (vis[ny][nx] == true || a[ny][nx] == 0) {
+            continue;
+          }
+          q.push({ny, nx});
+          vis[ny][nx] = true;
+        }
+      }
+
+      ans = std::max(ans, cnt);
+    }
   }
-  cout << ans << "\n";
+
+  std::cout << ans << "\n";
 }
