cd3f8978cff77f5ce27159f8d9bc8592ba009b92
kushinada <kushinada2077@gmail.com>
2025-04-05 15:34:59 +0900

    [Gold I] Title: 구간 합 구하기, Time: 184 ms, Memory: 18404 KB -BaekjoonHub

--- 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.cc
+++ 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.cc
@@ -1,55 +1,47 @@
-#include <algorithm>
-#include <climits>
-#include <deque>
-#include <iostream>
-#include <map>
-#include <numeric>
-#include <queue>
-#include <tuple>
-#include <vector>
-#define fastio cin.tie(0)->sync_with_stdio(0);
-#define for_in(n) for (int i = 0; i < n; ++i)
-#define si(x) int(x.size())
-#define all(x) (x).begin(), (x).end()
-#define pb(...) push_back(__VA_ARGS__)
-#define X first
-#define Y second
-#define ROOT 1
-using ll = long long;
-using namespace std;
+#include <bits/stdc++.h>
+using i64 = long long;
 
-ll n, m, k, p, q, r;
-vector<ll> a, BIT;
-ll add(int i) {
-  ll ret = 0;
-  while (i) {
-    ret += BIT[i];
-    i -= (i & -i);
-  }
-  return ret;
-}
-void update(int i, ll x) {
-  while (i <= n) {
-    BIT[i] += x;
-    i += (i & -i);
+const int MAX = 1 << 20;
+i64 seg[MAX << 1];
+void init() {
+  for (int i = MAX - 1; i != 0; --i) {
+    seg[i] = seg[2 * i] + seg[2 * i + 1];
   }
 }
+void add(int node, i64 x) {
+  node += MAX;
+  while (node != 0) {
+    seg[node] += x;
+    node /= 2;
+  }
+}
+i64 sum(int node, int l, int r, int st, int en) {
+  if (r <= st || en <= l) {
+    return 0LL;
+  }
+  if (l <= st && en <= r) {
+    return seg[node];
+  }
+  int mid = (st + en) / 2;
+  return sum(2 * node, l, r, st, mid) + sum(2 * node + 1, l, r, mid, en);
+}
+
 int main() {
-  fastio;
-  cin >> n >> m >> k;
-  a = vector<ll>(n + 1);
-  BIT = vector<ll>(n + 1);
-  for (int i = 1; i <= n; ++i) {
-    cin >> a[i];
-    update(i, a[i]);
+  std::cin.tie(nullptr)->sync_with_stdio(false);
+  int N, M, K;
+  std::cin >> N >> M >> K;
+  for (int i = 0; i < N; ++i) {
+    std::cin >> seg[MAX + i];
   }
-  for (int i = 0; i < k + m; ++i) {
-    cin >> p >> q >> r;
-    if (p == 1) {
-      update(q, r - a[q]);
-      a[q] = r;
-    } else {
-      cout << add(r) - add(q - 1) << "\n";
+  init();
+  for (int i = 0; i < M + K; ++i) {
+    i64 a, b, c;
+    std::cin >> a >> b >> c;
+    b--;
+    if (a == 1) {
+      add(b, c - seg[MAX + b]);
+    } else if (a == 2) {
+      std::cout << sum(1, b, c, 0, MAX) << "\n";
     }
   }
 }
