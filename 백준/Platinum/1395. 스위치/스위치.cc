68ceb4d6cdfd6c33a614f9eeb72364323075c2e9
kushinada <kushinada2077@gmail.com>
2025-04-17 17:45:14 +0900

    [Platinum III] Title: 스위치, Time: 76 ms, Memory: 3300 KB -BaekjoonHub

--- 백준/Platinum/1395. 스위치/스위치.cc
+++ 백준/Platinum/1395. 스위치/스위치.cc
@@ -1,74 +1,59 @@
-#include <algorithm>
-#include <climits>
-#include <deque>
-#include <iostream>
-#include <map>
-#include <numeric>
-#include <queue>
-#include <set>
-#include <stack>
-#include <tuple>
-#include <unordered_map>
-#include <unordered_set>
-#include <vector>
-#define PATH "/Users/leedongha/Downloads/PS/input.txt"
-#define fastio cin.tie(0)->sync_with_stdio(0);
-#define for_in(n) for (int i = 0; i < n; ++i)
-#define si(x) int(x.size())
-#define all(x) (x).begin(), (x).end()
-#define pb(...) push_back(__VA_ARGS__)
-#define X first
-#define Y second
-#define ROOT 1
-#define INF 0x3f3f3f3f
-using ll = long long;
-using namespace std;
+#include <bits/stdc++.h>
+using i64 = long long;
 
-const int _size = 1 << 17;
-int seg[_size << 1];
-bool lazy[_size << 1];
+const int MAX = 1 << 17;
+int info[MAX << 1];
+bool lazy[MAX << 1];
 
-void propagate(int node, int st, int en) {
-  if (lazy[node]) {
-    if (node < _size) {
-      lazy[node << 1] = !lazy[node << 1];
-      lazy[node << 1 | 1] = !lazy[node << 1 | 1];
+void propagate(int p, int l, int r) {
+  if (lazy[p] != 0) {
+    if (p < MAX) {
+      lazy[2 * p] = !lazy[2 * p];
+      lazy[2 * p + 1] = !lazy[2 * p + 1];
     }
-    seg[node] = (en - st) - seg[node];
-    lazy[node] = 0;
+    info[p] = (r - l) - info[p];
+    lazy[p] = false;
   }
 }
-int sum(int node, int st, int en, int l, int r) {
-  propagate(node, st, en);
-  if (en <= l || r <= st) return 0;
-  if (l <= st && en <= r) return seg[node];
-  int mid = (st + en) / 2;
-  return sum(node << 1, st, mid, l, r) + sum(node << 1 | 1, mid, en, l, r);
-}
-void update(int node, int st, int en, int l, int r) {
-  propagate(node, st, en);
-  if (en <= l || r <= st) return;
-  if (l <= st && en <= r) {
-    lazy[node] = !lazy[node];
-    propagate(node, st, en);
+void modify(int p, int l, int r, int x, int y) {
+  propagate(p, l, r);
+  if (r <= x || y <= l) {
     return;
   }
-  int mid = (st + en) / 2;
-  update(node << 1, st, mid, l, r);
-  update(node << 1 | 1, mid, en, l, r);
-  seg[node] = seg[node << 1] + seg[node << 1 | 1];
+  if (x <= l && r <= y) {
+    lazy[p] = !lazy[p];
+    propagate(p, l, r);
+    return;
+  }
+  int m = (l + r) / 2;
+  modify(2 * p, l, m, x, y);
+  modify(2 * p + 1, m, r, x, y);
+  info[p] = info[2 * p] + info[2 * p + 1];
+}
+int rangeQuery(int p, int l, int r, int x, int y) {
+  propagate(p, l, r);
+  if (r <= x || y <= l) {
+    return 0;
+  }
+  if (x <= l && r <= y) {
+    return info[p];
+  }
+  int m = (l + r) / 2;
+  return rangeQuery(2 * p, l, m, x, y) + rangeQuery(2 * p + 1, m, r, x, y);
 }
 int main() {
-  fastio;
-  int n, m, o, s, t;
-  cin >> n >> m;
-  for (int i = 0; i < m; ++i) {
-    cin >> o >> s >> t;
-    if (s > t) swap(s, t);
-    if (o) {
-      cout << sum(ROOT, 0, _size, s - 1, t) << "\n";
-    } else {
-      update(ROOT, 0, _size, s - 1, t);
+  std::cin.tie(nullptr)->sync_with_stdio(false);
+  int N, M;
+  std::cin >> N >> M;
+  for (int i = 0; i < M; ++i) {
+    int a, b, c;
+    std::cin >> a >> b >> c;
+    b--;
+    c--;
+    if (a == 0) {
+      modify(1, 0, MAX, b, c + 1);
+    } else if (a == 1) {
+      std::cout << rangeQuery(1, 0, MAX, b, c + 1) << "\n";
     }
   }
 }
