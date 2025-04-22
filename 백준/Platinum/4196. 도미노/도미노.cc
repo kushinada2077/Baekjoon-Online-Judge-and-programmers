a3e2cc29aab7482efc24d308eb372bdf2dc48f8d
kushinada <kushinada2077@gmail.com>
2025-04-22 17:03:51 +0900

    [Platinum IV] Title: 도미노, Time: 132 ms, Memory: 27376 KB -BaekjoonHub

--- 백준/Platinum/4196. 도미노/도미노.cc
+++ 백준/Platinum/4196. 도미노/도미노.cc
@@ -1,83 +1,76 @@
-#include <algorithm>
-#include <iostream>
-#include <queue>
-#include <stack>
-#include <vector>
-#define PATH "/Users/leedongha/Downloads/PS/input.txt"
-#define fastio cin.tie(0)->sync_with_stdio(0);
-#define for_in(n) for (int i = 0; i < n; ++i)
-#define si(x) int(x.size())
-#define all(x) (x).begin(), (x).end()
-#define pb(...) push_back(__VA_ARGS__)
-#define X first
-#define Y second
-#define ROOT 1
-#define INF 0x3f3f3f3f
-using ll = long long;
-using namespace std;
+#include <bits/stdc++.h>
+using i64 = long long;
 
-int t, n, m, u, v, dfsn[100001], SN, indg[100001], scc[100001], cnt = 1;
-bool finished[100001];
-vector<int> adj[100001];
-vector<pair<int, int>> edge;
-stack<int> s;
-int dfs(int cur) {
-  dfsn[cur] = cnt++;
-  s.push(cur);
-  int res = dfsn[cur];
-
-  for (auto& nxt : adj[cur]) {
-    if (dfsn[nxt] == 0) res = min(res, dfs(nxt));
-    else if (finished[nxt] == 0) res = min(res, dfsn[nxt]);
+void solve() {
+  int N, M;
+  std::cin >> N >> M;
+  std::vector adj(N, std::vector<int>());
+  for (int i = 0; i < M; ++i) {
+    int u, v;
+    std::cin >> u >> v;
+    adj[u - 1].push_back(v - 1);
   }
 
-  if (res == dfsn[cur]) {
-    while (1) {
-      int curr = s.top();
-      s.pop();
-      finished[curr] = 1;
-      scc[curr] = SN;
-      if (curr == cur) break;
+  int cnt = 0, SN = 0;
+  std::vector<int> dfsn(N), sn(N);
+  std::vector<bool> finished(N);
+  std::vector<std::vector<int>> SCC;
+  std::stack<int> S;
+  std::function<int(int)> dfs = [&](int curr) {
+    dfsn[curr] = ++cnt;
+    S.push(curr);
+    int result = dfsn[curr];
+    for (int next : adj[curr]) {
+      if (dfsn[next] == 0) {
+        result = std::min(result, dfs(next));
+      } else if (finished[next] == 0) {
+        result = std::min(result, dfsn[next]);
+      }
     }
-    SN++;
-  }
 
-  return res;
-}
-void init() {
-  cin >> n >> m;
-  for (int i = 0; i < n + 1; ++i) {
-    adj[i].clear();
-    dfsn[i] = indg[i] = scc[i] = finished[i] = 0;
+    if (result == dfsn[curr]) {
+      std::vector<int> currSCC;
+      while (true) {
+        int t = S.top();
+        S.pop();
+        currSCC.push_back(t);
+        finished[t] = true;
+        sn[t] = SN;
+        if (t == curr) {
+          break;
+        }
+      }
+      SCC.push_back(currSCC);
+      SN++;
+    }
+    return result;
+  };
+  for (int i = 0; i < N; ++i) {
+    if (dfsn[i] == 0) {
+      dfs(i);
+    }
   }
-  cnt = 1;
-  SN = 0;
-  edge.clear();
-  for (int i = 0; i < m; ++i) {
-    cin >> u >> v;
-    edge.pb({u, v});
-    adj[u].pb(v);
+  std::vector<int> indg(SN);
+  for (int u = 0; u < N; ++u) {
+    for (int v : adj[u]) {
+      if (sn[u] != sn[v]) {
+        indg[sn[v]]++;
+      }
+    }
   }
+  int ans = 0;
+  for (int i = 0; i < SN; ++i) {
+    if (indg[i] == 0) {
+      ans++;
+    }
+  }
+  std::cout << ans << "\n";
 }
 int main() {
-  fastio;
-  cin >> t;
-  while (t--) {
-    init();
-
-    for (int i = 1; i <= n; ++i) {
-      if (dfsn[i] == 0) dfs(i);
-    }
-
-    for (auto& [u, v] : edge) {
-      indg[scc[v]] += scc[u] != scc[v];
-    }
-
-    int ans = 0;
-    for (int i = 0; i < SN; ++i) {
-      if (indg[i] == 0) ans++;
-    }
-
-    cout << ans << "\n";
+  std::cin.tie(nullptr)->sync_with_stdio(false);
+  int T;
+  std::cin >> T;
+  while (T--) {
+    solve();
   }
 }
