b8d79fcfc1c71f8ec28e16ea82869a793b91be86
kushinada <kushinada2077@gmail.com>
2025-04-22 15:33:40 +0900

    [Platinum V] Title: Strongly Connected Component, Time: 20 ms, Memory: 3620 KB -BaekjoonHub

--- 백준/Platinum/2150. Strongly Connected Component/Strongly Connected Component.cc
+++ 백준/Platinum/2150. Strongly Connected Component/Strongly Connected Component.cc
@@ -1,66 +1,62 @@
-#include <algorithm>
-#include <iostream>
-#include <queue>
-#include <stack>
-#include <vector>
-#define PATH "/Users/leedongha/Downloads/PS/input.txt"
-#define fastio cin.tie(0)->sync_with_stdio(0);
-#define for_in(n) for (int i = 0; i < n; ++i)
-#define si(x) int(x.size())
-#define all(x) (x).begin(), (x).end()
-#define pb(...) push_back(__VA_ARGS__)
-#define X first
-#define Y second
-#define ROOT 1
-#define INF 0x3f3f3f3f
-using ll = long long;
-using namespace std;
+#include <bits/stdc++.h>
+using i64 = long long;
 
-int V, E, u, v, dfsn[10001], cnt = 1;
-vector<int> adj[10001];
-vector<vector<int>> scc;
-bool finished[10001];
-stack<int> s;
-int dfs(int cur) {
-  dfsn[cur] = cnt++;
-  int ret = dfsn[cur];
-  s.push(cur);
-  for (auto& nxt : adj[cur]) {
-    if (dfsn[nxt] == 0) ret = min(ret, dfs(nxt));
-    else if (!finished[nxt]) ret = min(ret, dfsn[nxt]);
-  }
+constexpr int MAX = 10000;
+int V, E, cnt, dfsn[MAX], SN, sn[MAX];
+std::vector<int> adj[MAX];
+bool finished[MAX];
+std::vector<std::vector<int>> SCC;
+std::stack<int> S;
+int dfs(int curr) {
+  dfsn[curr] = ++cnt;
+  S.push(curr);
 
-  if (ret == dfsn[cur]) {
-    vector<int> tmp;
-    while (1) {
-      int curr = s.top();
-      s.pop();
-      tmp.pb(curr);
-      finished[curr] = 1;
-      if (curr == cur) break;
+  int result = dfsn[curr];
+  for (int next : adj[curr]) {
+    if (dfsn[next] == 0) {
+      result = std::min(result, dfs(next));
+    } else if (finished[next] == 0) {
+      result = std::min(result, dfsn[next]);
     }
-    sort(all(tmp));
-    scc.pb(tmp);
   }
-  return ret;
+
+  if (result == dfsn[curr]) {
+    std::vector<int> currSCC;
+    while (true) {
+      int t = S.top();
+      S.pop();
+      currSCC.push_back(t);
+      finished[t] = true;
+      sn[t] = SN;
+      if (t == curr) {
+        break;
+      }
+    }
+    sort(currSCC.begin(), currSCC.end());
+    SCC.push_back(currSCC);
+    SN++;
+  }
+  return result;
 }
 int main() {
-  fastio;
-  cin >> V >> E;
+  std::cin.tie(nullptr)->sync_with_stdio(false);
+  std::cin >> V >> E;
   for (int i = 0; i < E; ++i) {
-    cin >> u >> v;
-    adj[u].pb(v);
+    int u, v;
+    std::cin >> u >> v;
+    adj[u - 1].push_back(v - 1);
   }
-
-  for (int i = 1; i <= V; ++i) {
-    if (dfsn[i]) continue;
-    dfs(i);
+  for (int i = 0; i < V; ++i) {
+    if (dfsn[i] == 0) {
+      dfs(i);
+    }
   }
-
-  sort(all(scc));
-  cout << si(scc) << "\n";
-  for (auto& sc : scc) {
-    for (auto& i : sc) cout << i << " ";
-    cout << "-1\n";
+  sort(SCC.begin(), SCC.end());
+  std::cout << SCC.size() << "\n";
+  for (auto scc : SCC) {
+    for (auto i : scc) {
+      std::cout << i + 1 << " ";
+    }
+    std::cout << "-1 \n";
   }
 }
