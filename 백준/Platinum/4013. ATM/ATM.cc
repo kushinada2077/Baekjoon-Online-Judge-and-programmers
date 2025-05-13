be5440b91c74d5f9accc747f9df4b0d93fb3a91a
kushinada <kushinada2077@gmail.com>
2025-05-13 16:18:12 +0900

    [Platinum II] Title: ATM, Time: 292 ms, Memory: 101616 KB -BaekjoonHub

--- 백준/Platinum/4013. ATM/ATM.cc
+++ 백준/Platinum/4013. ATM/ATM.cc
@@ -1,104 +1,109 @@
-#include <algorithm>
-#include <iostream>
-#include <queue>
-#include <stack>
-#include <vector>
-#define PATH "/Users/leedongha/Downloads/PS/input.txt"
-#define fastio cin.tie(0)->sync_with_stdio(0);
-#define for_in(n) for (int i = 0; i < n; ++i)
-#define si(x) int(x.size())
-#define all(x) (x).begin(), (x).end()
-#define pb(...) push_back(__VA_ARGS__)
-#define X first
-#define Y second
-#define ROOT 1
-#define INF 0x3f3f3f3f
-using ll = long long;
-using namespace std;
+#include <bits/stdc++.h>
+using i64 = long long;
 
-int n, m, u, v, st, p, cnt = 1, SN;
-int indg[500001], atm[500001], scc[500001], dfsn[500001], scc_sum[500001], ans[500001];
-bool has_r[500001], finished[500001], scc_has_r[500001], scc_poss[500001];
-vector<int> adj[500001], scc_adj[500001];
-vector<pair<int, int>> edge;
-stack<int> s;
-
-int dfs(int cur) {
-  dfsn[cur] = cnt++;
-  s.push(cur);
-  int res = dfsn[cur];
-
-  for (auto& nxt : adj[cur]) {
-    if (dfsn[nxt] == 0) res = min(res, dfs(nxt));
-    else if (finished[nxt] == 0) res = min(res, dfsn[nxt]);
+const int MAX = 500000;
+int N, M, S, P;
+std::vector adj(MAX, std::vector<int>());
+int cash[MAX], dfsn[MAX], cashSCC[MAX], cashAcc[MAX], SCC[MAX], indg[MAX];
+bool restaur[MAX], restaurSCC[MAX], finished[MAX];
+int cnt, SN;
+std::stack<int> ST;
+int dfs(int u) {
+  dfsn[u] = ++cnt;
+  ST.push(u);
+  int result = dfsn[u];
+  for (auto v : adj[u]) {
+    if (dfsn[v] == 0) {
+      result = std::min(result, dfs(v));
+    } else if (finished[v] == 0) {
+      result = std::min(result, dfsn[v]);
+    }
   }
 
-  if (res == dfsn[cur]) {
-    while (1) {
-      int curr = s.top();
-      s.pop();
-      finished[curr] = 1;
-      scc[curr] = SN;
-      scc_sum[SN] += atm[curr];
-      if (scc_has_r[SN] == 0 && has_r[curr]) scc_has_r[SN] = 1;
-      if (curr == cur) break;
+  if (result == dfsn[u]) {
+    while (true) {
+      int t = ST.top();
+      ST.pop();
+      cashSCC[SN] += cash[t];
+      finished[t] = true;
+      SCC[t] = SN;
+      if (restaur[t] == true && restaurSCC[SN] == false) {
+        restaurSCC[SN] = true;
+      }
+      if (t == u) {
+        break;
+      }
     }
     SN++;
   }
-  return res;
+  return result;
 }
-
 int main() {
-  fastio;
-  cin >> n >> m;
-  for (int i = 0; i < m; ++i) {
-    cin >> u >> v;
-    adj[u].pb(v);
-    edge.pb({u, v});
+  std::cin.tie(nullptr)->sync_with_stdio(false);
+  std::cin >> N >> M;
+  for (int i = 0; i < M; ++i) {
+    int u, v;
+    std::cin >> u >> v;
+    adj[u - 1].push_back(v - 1);
+  }
+  for (int i = 0; i < N; ++i) {
+    std::cin >> cash[i];
+  }
+  std::cin >> S >> P;
+  S--;
+  for (int i = 0; i < P; ++i) {
+    int x;
+    std::cin >> x;
+    restaur[x - 1] = true;
   }
 
-  for (int i = 1; i <= n; ++i) cin >> atm[i];
-  cin >> st >> p;
-  for (int i = 0; i < p; ++i) {
-    cin >> u;
-    has_r[u] = 1;
-  }
-
-  for (int i = 1; i <= n; ++i) {
-    if (dfsn[i]) continue;
-    dfs(i);
-  }
-
-  for (auto& [u, v] : edge) {
-    if (scc[u] != scc[v]) {
-      scc_adj[scc[u]].pb(scc[v]);
-      indg[scc[v]]++;
+  for (int i = 0; i < N; ++i) {
+    if (dfsn[i] == 0) {
+      dfs(i);
     }
   }
 
-  queue<int> q;
-  for (int i = 0; i < SN; ++i) {
-    ans[i] = scc_sum[i];
-    if (scc[st] == i) scc_poss[i] = 1;
-    if (indg[i] == 0) q.push(i);
-  }
-
-  while (!q.empty()) {
-    int cur = q.front();
-    q.pop();
-
-    for (auto& nxt : scc_adj[cur]) {
-      if (scc_poss[cur]) {
-        scc_poss[nxt] = 1;
-        ans[nxt] = max(ans[nxt], ans[cur] + scc_sum[nxt]);
+  std::vector sAdj(MAX, std::vector<int>());
+  bool isPoss[MAX];
+  for (int u = 0; u < N; ++u) {
+    for (int v : adj[u]) {
+      if (SCC[u] != SCC[v]) {
+        sAdj[SCC[u]].push_back(SCC[v]);
+        indg[SCC[v]]++;
       }
-      if (--indg[nxt] == 0) q.push(nxt);
     }
   }
 
-  int anss = -1;
+  std::queue<int> Q;
   for (int i = 0; i < SN; ++i) {
-    if (scc_poss[i] && scc_has_r[i]) anss = max(anss, ans[i]);
+    cashAcc[i] = cashSCC[i];
+    if (SCC[S] == i) {
+      isPoss[i] = true;
+    }
+    if (indg[i] == 0) {
+      Q.push(i);
+    }
   }
-  cout << anss << "\n";
+
+  while (!Q.empty()) {
+    int u = Q.front();
+    Q.pop();
+    for (auto v : sAdj[u]) {
+      if (isPoss[u]) {
+        isPoss[v] = true;
+        cashAcc[v] = std::max(cashAcc[v], cashAcc[u] + cashSCC[v]);
+      }
+      if (--indg[v] == 0) {
+        Q.push(v);
+      }
+    }
+  }
+
+  int ans = -1;
+  for (int i = 0; i < SN; ++i) {
+    if (restaurSCC[i] == true && isPoss[i] == true) {
+      ans = std::max(ans, cashAcc[i]);
+    }
+  }
+  std::cout << ans << "\n";
 }
