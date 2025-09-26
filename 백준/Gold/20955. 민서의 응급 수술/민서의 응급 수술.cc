ed84749e2f772a53e2e2a80125fe3a995ab5af5b
kushinada <kushinada2077@gmail.com>
2025-09-26 16:13:55 +0900

    [Gold IV] Title: 민서의 응급 수술, Time: 44 ms, Memory: 10104 KB -BaekjoonHub

--- 백준/Gold/20955. 민서의 응급 수술/민서의 응급 수술.cc
+++ 백준/Gold/20955. 민서의 응급 수술/민서의 응급 수술.cc
@@ -2,11 +2,53 @@
 using i64 = long long;
 using P = std::pair<int, int>;
 
+struct DSU {
+  std::vector<int> f, siz;
+
+  DSU() {}
+  DSU(int n) {
+    init(n);
+  }
+
+  void init(int n) {
+    f.resize(n);
+    std::iota(f.begin(), f.end(), 0);
+    siz.assign(n, 1);
+  }
+
+  int find(int x) {
+    while (x != f[x]) {
+      x = f[x] = f[f[x]];
+    }
+    return x;
+  }
+
+  bool same(int x, int y) {
+    return find(x) == find(y);
+  }
+
+  bool merge(int x, int y) {
+    x = find(x), y = find(y);
+    if (x == y) return false;
+    if (siz[x] < siz[y]) {
+      std::swap(x, y);
+    }
+    siz[x] += siz[y];
+    f[y] = x;
+    return true;
+  }
+
+  int size(int x) {
+    return siz[find(x)];
+  }
+};
+
 int main() {
   std::cin.tie(nullptr)->sync_with_stdio(false);
   int n, m;
   std::cin >> n >> m;
   std::vector adj(n, std::vector<int>());
+  std::vector<P> edge(m);
   for (int i = 0; i < m; ++i) {
     int u, v;
     std::cin >> u >> v;
@@ -14,34 +56,30 @@ int main() {
     v--;
     adj[u].push_back(v);
     adj[v].push_back(u);
+    edge[i] = {u, v};
   }
 
-  std::vector<std::vector<int>> component;
+  int component = 0;
   std::vector<bool> vis(n);
   auto dfs = [&](auto&& dfs, int u) {
     if (vis[u] == true) return;
-    component.back().push_back(u);
     vis[u] = true;
     for (auto v : adj[u]) dfs(dfs, v);
   };
 
   for (int i = 0; i < n; ++i) {
     if (vis[i] == false) {
-      component.push_back({});
+      component++;
       dfs(dfs, i);
     }
   }
 
-  i64 ans = (int)component.size() - 1;
-  for (int i = 0; i < (int)component.size(); ++i) {
-    int v = (int)component[i].size(), e = 0;
-    for (auto u : component[i]) {
-      e += (int)adj[u].size();
-    }
+  int ans = component - 1;
+  DSU dsu(n);
 
-    e /= 2;
-
-    ans += std::abs((v - 1) - e);
+  for (auto [u, v] : edge) {
+    if (dsu.same(u, v)) ans++;
+    else dsu.merge(u, v);
   }
 
   std::cout << ans << "\n";
