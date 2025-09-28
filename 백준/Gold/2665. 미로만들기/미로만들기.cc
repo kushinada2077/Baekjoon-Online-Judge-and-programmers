7461c5a6c4c7ccafaa89ccb482839cf98dc56c6d
kushinada <kushinada2077@gmail.com>
2025-09-29 01:11:18 +0900

    [Gold IV] Title: 미로만들기, Time: 0 ms, Memory: 2160 KB -BaekjoonHub

--- 백준/Gold/2665. 미로만들기/미로만들기.cc
+++ 백준/Gold/2665. 미로만들기/미로만들기.cc
@@ -13,33 +13,29 @@ int main() {
   }
 
   const int INF = 0x3f3f3f3f;
-  std::vector cnt(n, std::vector<int>(n, INF));
-  std::queue<P> q;
-  q.push({0, 0});
-  cnt[0][0] = 0;
+  std::vector dist(n, std::vector<int>(n, INF));
+  std::deque<P> dq;
+  dq.push_back({0, 0});
+  dist[0][0] = 0;
 
-  while (!q.empty()) {
-    auto [y, x] = q.front();
-    q.pop();
+  while (!dq.empty()) {
+    auto [y, x] = dq.front();
+    dq.pop_front();
 
     for (int dir = 0; dir < 4; ++dir) {
       int ny = y + "2101"[dir] - '1';
       int nx = x + "1210"[dir] - '1';
       if (ny < 0 || ny >= n || nx < 0 || nx >= n) continue;
-      if (board[ny][nx] == '0') {
-        if (cnt[ny][nx] > cnt[y][x] + 1) {
-          q.push({ny, nx});
-          cnt[ny][nx] = cnt[y][x] + 1;
-        }
+      if (board[ny][nx] == '1' && dist[ny][nx] > dist[y][x]) {
+        dq.push_front({ny, nx});
+        dist[ny][nx] = dist[y][x];
       }
-      if (board[ny][nx] == '1') {
-        if (cnt[ny][nx] > cnt[y][x]) {
-          q.push({ny, nx});
-          cnt[ny][nx] = cnt[y][x];
-        }
+      if (board[ny][nx] == '0' && dist[ny][nx] > dist[y][x] + 1) {
+        dq.push_back({ny, nx});
+        dist[ny][nx] = dist[y][x] + 1;
       }
     }
   }
 
-  std::cout << cnt[n - 1][n - 1] << "\n";
+  std::cout << dist[n - 1][n - 1] << "\n";
 }
