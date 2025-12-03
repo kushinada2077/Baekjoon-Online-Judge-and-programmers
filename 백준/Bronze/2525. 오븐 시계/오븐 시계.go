a3f62a49a78ff24e626ed85ea1b07a8d50b987e6
kushinada <kushinada2077@gmail.com>
2025-12-03 17:14:33 +0900

    [Bronze III] Title: 오븐 시계, Time: 4 ms, Memory: 988 KB -BaekjoonHub

---
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+const PATH = "/Users/leedongha/Downloads/PS/input.txt"
+
+func init() {
+	if file, err := os.Open(PATH); err == nil {
+		os.Stdin = file
+	}
+}
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	writer := bufio.NewWriter(os.Stdout)
+	defer writer.Flush()
+
+	var a, b, c int
+	fmt.Fscan(reader, &a, &b, &c)
+	a = (a + (b+c)/60) % 24
+	b = (b + c) % 60
+	fmt.Println(a, b)
+}
