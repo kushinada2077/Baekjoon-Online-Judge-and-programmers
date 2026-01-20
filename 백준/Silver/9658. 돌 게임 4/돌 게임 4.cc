be914ef1e2177c7e03635ab4d921d9b333e3ea1f
kushinada <kushinada2077@gmail.com>
2026-01-20 16:58:19 +0900

    [Silver II] Title: 돌 게임 4, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/9658. 돌 게임 4/돌 게임 4.cc
+++ 백준/Silver/9658. 돌 게임 4/돌 게임 4.cc
@@ -15,27 +15,14 @@ int main() {
   kushinada;
   int n;
   std::cin >> n;
-
-  std::vector ans = {"SK", "CY"};
-  std::vector d = {1, 3, 4};
-  std::vector dp(n + 1, std::vector<int>(2, -1));
-  dp[1][0] = 1;
-  dp[1][1] = 0;
+  std::vector ans = {"CY", "SK"};
+  std::vector<bool> dp(n + 1);
 
   for (int i = 2; i <= n; ++i) {
-    for (int j = 0; j < 2; ++j) {
-      if (j == 0) dp[i][j] = 1;
-      for (int k = 0; k < 3; ++k) {
-        if (i - d[k] > 0) {
-          if (j == 0) {
-            dp[i][j] = std::min(dp[i][j], dp[i - d[k]][(j + 1) % 2]);
-          } else {
-            dp[i][j] = std::max(dp[i][j], dp[i - d[k]][(j + 1) % 2]);
-          }
-        }
-      }
+    for (int k : {1, 3, 4}) {
+      if (i - k > 0 && dp[i - k] == false) dp[i] = true;
     }
   }
 
-  std::cout << ans[dp[n][0]] << "\n";
+  std::cout << ans[dp[n]] << "\n";
 }
