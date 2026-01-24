d800899711ab3ace9d248a22b20146e24c1d5342
kushinada <kushinada2077@gmail.com>
2026-01-24 15:21:12 +0900

    [Silver III] Title: 이전 순열, Time: 0 ms, Memory: 2180 KB -BaekjoonHub

---
+++ 백준/Silver/10973. 이전 순열/이전 순열.cc
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+#ifndef ONLINE_JUDGE
+#define kushinada freopen(getenv("MY_PATH"), "r", stdin);
+#else
+#define kushinada
+#endif
+
+using i64 = long long;
+using P = std::pair<int, int>;
+using T = std::tuple<int, int, int>;
+
+int main() {
+  std::cin.tie(nullptr)->sync_with_stdio(false);
+  kushinada;
+  int n;
+  std::cin >> n;
+  std::vector<int> a(n);
+  for (int i = 0; i < n; ++i) {
+    std::cin >> a[i];
+  }
+
+  bool chk = std::prev_permutation(a.begin(), a.end());
+
+  if (chk == false) {
+    std::cout << "-1\n";
+  } else {
+    for (auto x : a) std::cout << x << " ";
+  }
+}
