611b401bdae3487364560e8a68d46153ef318a42
kushinada <kushinada2077@gmail.com>
2026-02-06 19:10:00 +0900

    [Platinum V] Title: Strongly Connected Component, Time: 20 ms, Memory: 3584 KB -BaekjoonHub

--- 백준/Platinum/2150. Strongly Connected Component/Strongly Connected Component.cc
+++ 백준/Platinum/2150. Strongly Connected Component/Strongly Connected Component.cc
@@ -1,62 +1,66 @@
 #include <bits/stdc++.h>
+#ifndef ONLINE_JUDGE
+#define kushinada freopen(getenv("MY_PATH"), "r", stdin);
+#else
+#define kushinada
+#endif
+
 using i64 = long long;
+using P = std::pair<int, int>;
+using T = std::tuple<int, int, int>;
 
-constexpr int MAX = 10000;
-int V, E, cnt, dfsn[MAX], SN, sn[MAX];
-std::vector<int> adj[MAX];
-bool finished[MAX];
-std::vector<std::vector<int>> SCC;
-std::stack<int> S;
-int dfs(int curr) {
-  dfsn[curr] = ++cnt;
-  S.push(curr);
-
-  int result = dfsn[curr];
-  for (int next : adj[curr]) {
-    if (dfsn[next] == 0) {
-      result = std::min(result, dfs(next));
-    } else if (finished[next] == 0) {
-      result = std::min(result, dfsn[next]);
-    }
-  }
-
-  if (result == dfsn[curr]) {
-    std::vector<int> currSCC;
-    while (true) {
-      int t = S.top();
-      S.pop();
-      currSCC.push_back(t);
-      finished[t] = true;
-      sn[t] = SN;
-      if (t == curr) {
-        break;
-      }
-    }
-    sort(currSCC.begin(), currSCC.end());
-    SCC.push_back(currSCC);
-    SN++;
-  }
-  return result;
-}
 int main() {
   std::cin.tie(nullptr)->sync_with_stdio(false);
-  std::cin >> V >> E;
-  for (int i = 0; i < E; ++i) {
-    int u, v;
-    std::cin >> u >> v;
-    adj[u - 1].push_back(v - 1);
+  kushinada;
+  int v, e;
+  std::cin >> v >> e;
+  std::vector adj(v, std::vector<int>());
+  for (int i = 0; i < e; ++i) {
+    int a, b;
+    std::cin >> a >> b;
+    adj[a - 1].push_back(b - 1);
   }
-  for (int i = 0; i < V; ++i) {
-    if (dfsn[i] == 0) {
-      dfs(i);
+
+  std::stack<int> s;
+  int dfsn_cnt = 0;
+  std::vector<int> dfsn(v, -1);
+  std::vector<std::vector<int>> scc;
+  std::vector<bool> finished(v);
+  auto dfs = [&](auto&& dfs, int u) -> int {
+    dfsn[u] = dfsn_cnt++;
+    s.push(u);
+    int res = dfsn[u];
+
+    for (auto v : adj[u]) {
+      if (dfsn[v] == -1) res = std::min(res, dfs(dfs, v));
+      else if (!finished[v]) res = std::min(res, dfsn[v]);
     }
+
+    if (res == dfsn[u]) {
+      std::vector<int> tmp;
+      while (!s.empty()) {
+        int p = s.top();
+        s.pop();
+        finished[p] = true;
+        tmp.push_back(p);
+        if (p == u) break;
+      }
+
+      std::sort(tmp.begin(), tmp.end());
+      scc.push_back(tmp);
+    }
+
+    return res;
+  };
+
+  for (int i = 0; i < v; ++i) {
+    if (dfsn[i] == -1) dfs(dfs, i);
   }
-  sort(SCC.begin(), SCC.end());
-  std::cout << SCC.size() << "\n";
-  for (auto scc : SCC) {
-    for (auto i : scc) {
-      std::cout << i + 1 << " ";
-    }
-    std::cout << "-1 \n";
+
+  std::sort(scc.begin(), scc.end(), [&](auto& a, auto& b) { return a.front() < b.front(); });
+  std::cout << scc.size() << "\n";
+  for (auto& curSCC : scc) {
+    for (auto u : curSCC) std::cout << u + 1 << " ";
+    std::cout << "-1\n";
   }
 }
