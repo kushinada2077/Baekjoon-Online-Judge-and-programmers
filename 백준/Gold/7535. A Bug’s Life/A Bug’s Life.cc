11c564828227d70064a14c463396d625cc2cfbd9
kushinada <kushinada2077@gmail.com>
2026-02-11 16:45:57 +0900

    [Gold III] Title: A Bug’s Life, Time: 220 ms, Memory: 18376 KB -BaekjoonHub

--- 백준/Gold/7535. A Bug’s Life/A Bug’s Life.cc
+++ 백준/Gold/7535. A Bug’s Life/A Bug’s Life.cc
@@ -1,94 +1,88 @@
-#include <algorithm>
-#include <iostream>
-#include <map>
-#include <set>
-#include <stack>
-#include <vector>
-#define PATH "/Users/leedongha/Downloads/PS/input.txt"
-#define L_PATH "input.txt"
-#define fastio cin.tie(0)->sync_with_stdio(0);
-#define rep(n) for (int i = 0; i < n; ++i)
-#define si(x) int(x.size())
-#define all(x) (x).begin(), (x).end()
-#define pb(...) push_back(__VA_ARGS__)
-#define X first
-#define Y second
-#define ROOT 1
-#define INF 0x3f3f3f3f
-using namespace std;
-using ll = long long;
-using T = tuple<int, int, int>;
-using P = pair<int, int>;
+#include <bits/stdc++.h>
+#ifndef ONLINE_JUDGE
+#define kushinada freopen(getenv("MY_PATH"), "r", stdin);
+#else
+#define kushinada
+#endif
 
-inline int oppo(int v) { return v % 2 ? v + 1 : v - 1; }
-const int MX = 2001;
-int t, cnt, SN;
-int dfsn[MX << 1], sn[MX << 1];
-bool finished[MX << 1];
-vector<int> adj[MX << 1];
-stack<int> s;
+using i64 = long long;
+using P = std::pair<int, int>;
+using T = std::tuple<int, int, int>;
 
-void init() {
-  cnt = SN = 0;
-  for (int i = 0; i < (MX << 1); ++i) {
-    dfsn[i] = sn[i] = finished[i] = 0;
-    adj[i].clear();
-  }
-}
+struct SCC {
+  int n;
+  int cur, cnt;
+  std::vector<std::vector<int>> adj;
+  std::vector<int> dfn, low, bel;
+  std::vector<int> stk;
 
-int dfs(int cur) {
-  int res = dfsn[cur] = ++cnt;
-  s.push(cur);
+  SCC(int n) : n(n), cur(0), cnt(0), adj(n), dfn(n, -1), low(n), bel(n, -1) {}
 
-  for (auto& nxt : adj[cur]) {
-    if (dfsn[nxt] == 0) res = min(res, dfs(nxt));
-    else if (finished[nxt] == 0) res = min(res, dfsn[nxt]);
-  }
+  void addEdge(int u, int v) { adj[u].push_back(v); }
 
-  if (res == dfsn[cur]) {
-    while (1) {
-      int v = s.top();
-      s.pop();
-      finished[v] = 1;
-      sn[v] = SN;
-      if (cur == v) break;
-    }
-    SN++;
-  }
+  void dfs(int u) {
+    dfn[u] = low[u] = cur++;
+    stk.push_back(u);
 
-  return res;
-}
-
-int main() {
-  fastio;
-  cin >> t;
-  for (int n, m, i{}; i < t; ++i) {
-    init();
-    cin >> n >> m;
-    for (int a, b; m--;) {
-      cin >> a >> b;
-      a = a > 0 ? 2 * a : 2 * -a - 1;
-      b = b > 0 ? 2 * b : 2 * -b - 1;
-      adj[oppo(a)].pb(b);
-      adj[a].pb(oppo(b));
-      adj[oppo(b)].pb(a);
-      adj[b].pb(oppo(a));
-    }
-
-    for (int i = 1; i <= 2 * n; ++i) {
-      if (dfsn[i] == 0) dfs(i);
-    }
-
-    bool ans = 1;
-    for (int i = 1; i <= n; ++i) {
-      if (sn[2 * i] == sn[2 * i - 1]) {
-        ans = 0;
-        break;
+    for (auto v : adj[u]) {
+      if (dfn[v] == -1) {
+        dfs(v);
+        low[u] = std::min(low[u], low[v]);
+      } else if (bel[v] == -1) {
+        low[u] = std::min(low[u], dfn[v]);
       }
     }
 
-    cout << "Scenario #" << i + 1 << ":\n";
-    if (ans) cout << "No suspicious bugs found!\n\n";
-    else cout << "Suspicious bugs found!\n\n";
+    if (dfn[u] == low[u]) {
+      int v;
+      do {
+        v = stk.back();
+        stk.pop_back();
+        bel[v] = cnt;
+      } while (v != u);
+      cnt++;
+    }
+  }
+
+  std::vector<int> work() {
+    for (int i = 0; i < n; ++i) {
+      if (dfn[i] == -1) dfs(i);
+    }
+    return bel;
+  }
+};
+
+int main() {
+  std::cin.tie(nullptr)->sync_with_stdio(false);
+  kushinada;
+  int t, cnt = 0;
+  std::cin >> t;
+  while (t--) {
+    [&] {
+      std::cout << "Scenario #" << ++cnt << ":\n";
+      int n, m;
+      std::cin >> n >> m;
+      SCC scc(2 * n);
+      for (int i = 0; i < m; ++i) {
+        int u, v;
+        std::cin >> u >> v;
+        u = (u - 1) * 2;
+        v = (v - 1) * 2;
+        scc.addEdge(u ^ 1, v);
+        scc.addEdge(v ^ 1, u);
+        scc.addEdge(u, v ^ 1);
+        scc.addEdge(v, u ^ 1);
+      }
+
+      auto bel = scc.work();
+      for (int i = 0; i < n; ++i) {
+        if (bel[2 * i] == bel[2 * i + 1]) {
+          std::cout << "Suspicious bugs found!\n\n";
+          return;
+        }
+      }
+
+      std::cout << "No suspicious bugs found!\n\n";
+    }();
   }
 }
