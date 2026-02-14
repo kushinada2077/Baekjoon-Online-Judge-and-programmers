e331f006fec7f624d6a1fe2ef09e190149121f0f
kushinada <kushinada2077@gmail.com>
2026-02-14 14:07:06 +0900

    [Gold I] Title: 합성함수와 쿼리, Time: 228 ms, Memory: 17700 KB -BaekjoonHub

--- 백준/Gold/17435. 합성함수와 쿼리/합성함수와 쿼리.cc
+++ 백준/Gold/17435. 합성함수와 쿼리/합성함수와 쿼리.cc
@@ -1,44 +1,44 @@
-#include <algorithm>
-#include <climits>
-#include <deque>
-#include <iostream>
-#include <map>
-#include <numeric>
-#include <queue>
-#include <set>
-#include <tuple>
-#include <vector>
-#define fastio cin.tie(0)->sync_with_stdio(0);
-#define for_in(n) for (int i = 0; i < n; ++i)
-#define si(x) int(x.size())
-#define all(x) (x).begin(), (x).end()
-#define pb(...) push_back(__VA_ARGS__)
-#define X first
-#define Y second
-#define ROOT 1
-using ll = long long;
-using namespace std;
+#include <bits/stdc++.h>
+#ifndef ONLINE_JUDGE
+#define kushinada freopen(getenv("MY_PATH"), "r", stdin);
+#else
+#define kushinada
+#endif
 
-int m, q, n, x;
-int nxt[500005][19];
+using i64 = long long;
+using P = std::pair<int, int>;
+using T = std::tuple<int, int, int>;
+
 int main() {
-  fastio;
-  cin >> m;
-  for (int i = 1; i <= m; ++i) cin >> nxt[i][0];
-
-  for (int j = 1; j < 19; ++j) {
-    for (int i = 1; i <= m; ++i) nxt[i][j] = nxt[nxt[i][j - 1]][j - 1];
+  std::cin.tie(nullptr)->sync_with_stdio(false);
+  kushinada;
+  int m;
+  std::cin >> m;
+  int b = 0;
+  while ((1 << b) <= 500000) b++;
+  std::vector f(b, std::vector<int>(m, -1));
+  for (int i = 0; i < m; ++i) {
+    std::cin >> f[0][i];
+    f[0][i]--;
   }
 
-  cin >> q;
-  while (q--) {
-    cin >> n >> x;
-    for (int i = 18; i >= 0; --i) {
-      if (n >= 1 << i) {
-        n -= 1 << i;
-        x = nxt[x][i];
-      }
+  for (int k = 1; k < b; ++k) {
+    for (int i = 0; i < m; ++i) {
+      f[k][i] = f[k - 1][f[k - 1][i]];
     }
-    cout << x << "\n";
+  }
+
+  int q;
+  std::cin >> q;
+  for (int i = 0; i < q; ++i) {
+    int n, x;
+    std::cin >> n >> x;
+    x--;
+    for (int cur = 0; n; n /= 2, cur++) {
+      if (n % 2 == 0) continue;
+      x = f[cur][x];
+    }
+
+    std::cout << x + 1 << "\n";
   }
 }
