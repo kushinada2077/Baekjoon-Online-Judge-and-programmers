8a134038f25ae5dfab1c21f5ac8d6a0ca88db74a
kushinada <kushinada2077@gmail.com>
2026-03-10 12:32:48 +0900

    [Gold V] Title: 공통 부분 문자열, Time: 72 ms, Memory: 65164 KB -BaekjoonHub

---
+++ 백준/Gold/5582. 공통 부분 문자열/공통 부분 문자열.cc
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+#ifndef ONLINE_JUDGE
+#define kushinada freopen(getenv("MY_PATH"), "r", stdin);
+#else
+#define kushinada
+#endif
+
+using i64 = long long;
+using P = std::pair<int, int>;
+using T = std::tuple<int, int, int>;
+
+int main() {
+  std::cin.tie(nullptr)->sync_with_stdio(false);
+  kushinada;
+  std::string s1, s2;
+  std::cin >> s1 >> s2;
+  int n = s1.size(), m = s2.size();
+  std::vector dp(n + 1, std::vector<int>(m + 1));
+  int ans = 0;
+  for (int i = 1; i <= n; ++i) {
+    for (int j = 1; j <= m; ++j) {
+      if (s1[i - 1] == s2[j - 1]) {
+        dp[i][j] = dp[i - 1][j - 1] + 1;
+        ans = std::max(ans, dp[i][j]);
+      }
+    }
+  }
+  std::println("{}", ans);
+}
