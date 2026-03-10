47d5bdf340ff7782142964da9f2eb6a942b5e6cf
kushinada <kushinada2077@gmail.com>
2026-03-10 15:25:32 +0900

    [Gold IV] Title: 여행 가자, Time: 0 ms, Memory: 2380 KB -BaekjoonHub

--- 백준/Gold/1976. 여행 가자/여행 가자.cc
+++ 백준/Gold/1976. 여행 가자/여행 가자.cc
@@ -8,48 +8,70 @@
 using i64 = long long;
 using P = std::pair<int, int>;
 
+struct DSU {
+  std::vector<int> f, siz;
+
+  DSU() {}
+  DSU(int n) { init(n); }
+
+  void init(int n) {
+    f.resize(n);
+    std::iota(f.begin(), f.end(), 0);
+    siz.assign(n, 1);
+  }
+
+  int find(int x) {
+    while (x != f[x]) {
+      x = f[x] = f[f[x]];
+    }
+    return x;
+  }
+
+  bool same(int x, int y) { return find(x) == find(y); }
+
+  bool merge(int x, int y) {
+    x = find(x), y = find(y);
+    if (x == y) return false;
+    if (siz[x] < siz[y]) {
+      std::swap(x, y);
+    }
+    siz[x] += siz[y];
+    f[y] = x;
+    return true;
+  }
+
+  int size(int x) { return siz[find(x)]; }
+};
+
 int main() {
   std::cin.tie(nullptr)->sync_with_stdio(false);
   kushinada;
   int n, m;
   std::cin >> n >> m;
-  std::vector adj(n, std::vector<int>(n));
+  DSU dsu(n);
+
   for (int i = 0; i < n; ++i) {
     for (int j = 0; j < n; ++j) {
-      std::cin >> adj[i][j];
-    }
-  }
-
-  for (int k = 0; k < n; ++k) {
-    for (int i = 0; i < n; ++i) {
-      for (int j = 0; j < n; ++j) {
-        if (adj[i][k] && adj[k][j]) adj[i][j] = 1;
-      }
-    }
-  }
-
-  for (int i = 0; i < n; ++i) {
-    adj[i][i] = 1;
-  }
-
-  bool ans = [&] -> bool {
-    int pre;
-    std::cin >> pre;
-    pre--;
-    for (int i = 0; i < m - 1; ++i) {
       int x;
       std::cin >> x;
-      x--;
-      if (!adj[pre][x]) {
-        return false;
+      if (x) {
+        dsu.merge(i, j);
       }
     }
-    return true;
-  }();
-
-  if (ans) {
-    std::println("YES");
-  } else {
-    std::println("NO");
   }
+
+  int s;
+  std::cin >> s;
+  s--;
+  for (int i = 0; i < m; ++i) {
+    int x;
+    std::cin >> x;
+    x--;
+    if (!dsu.same(s, x)) {
+      std::println("NO");
+      return 0;
+    }
+  }
+
+  std::println("YES");
 }
