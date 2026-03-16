477356c6c48d8c7bf2c8f27003a7b172cfe6062c
kushinada <kushinada2077@gmail.com>
2026-03-16 15:47:32 +0900

    [Gold I] Title: 냅색문제, Time: 8 ms, Memory: 3276 KB -BaekjoonHub

---
+++ 백준/Gold/1450. 냅색문제/냅색문제.cc
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+#ifndef ONLINE_JUDGE
+#define kushinada freopen(getenv("MY_PATH"), "r", stdin);
+#else
+#define kushinada
+#endif
+
+using i64 = long long;
+using P = std::pair<int, int>;
+using TP = std::tuple<int, int, int>;
+
+int main() {
+  std::cin.tie(nullptr)->sync_with_stdio(false);
+  kushinada;
+  int n, c;
+  std::cin >> n >> c;
+  std::vector<i64> a, b;
+  for (int i = 0; i < n / 2; ++i) {
+    i64 x;
+    std::cin >> x;
+    a.push_back(x);
+  }
+  for (int i = n / 2; i < n; ++i) {
+    i64 x;
+    std::cin >> x;
+    b.push_back(x);
+  }
+
+  int p = a.size(), q = b.size();
+  std::vector<i64> aa, bb;
+  for (int brute = 0; brute < (1 << p); ++brute) {
+    i64 sum = 0;
+    for (int i = 0; i < p; ++i) {
+      if (brute & (1 << i)) {
+        sum += a[i];
+      }
+    }
+    aa.push_back(sum);
+  }
+
+  for (int brute = 0; brute < (1 << q); ++brute) {
+    i64 sum = 0;
+    for (int i = 0; i < q; ++i) {
+      if (brute & (1 << i)) {
+        sum += b[i];
+      }
+    }
+    bb.push_back(sum);
+  }
+
+  std::ranges::sort(bb);
+  i64 ans = 0;
+  for (auto xa : aa) {
+    auto i = std::upper_bound(bb.begin(), bb.end(), c - xa) - bb.begin();
+    ans += i;
+  }
+  std::println("{}", ans);
+}
