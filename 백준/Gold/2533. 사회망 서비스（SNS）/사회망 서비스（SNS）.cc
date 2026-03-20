668a82dbd3d1ac8aaadbb1bf0dc7d02179aa2c6a
kushinada <kushinada2077@gmail.com>
2026-03-20 14:03:40 +0900

    [Gold III] Title: 사회망 서비스(SNS), Time: 1080 ms, Memory: 110584 KB -BaekjoonHub

--- 백준/Gold/2533. 사회망 서비스（SNS）/사회망 서비스（SNS）.cc
+++ 백준/Gold/2533. 사회망 서비스（SNS）/사회망 서비스（SNS）.cc
@@ -23,24 +23,10 @@ int main() {
     adj[u].push_back(v);
     adj[v].push_back(u);
   }
-  const int N = n + 1, IMP = 0x3f3f3f3f;
-  std::vector dp(N, std::vector<int>(2, IMP));
-  std::vector<int> p(N, -1);
-  std::queue<int> que;
-  que.push(n);
-  adj.push_back({0});
-  while (!que.empty()) {
-    int u = que.front();
-    que.pop();
-    for (auto v : adj[u]) {
-      if (p[v] == -1) {
-        p[v] = u;
-        que.push(v);
-      }
-    }
-  }
+  const int IMP = 0x3f3f3f3f;
+  std::vector dp(n, std::vector<int>(2, IMP));
 
-  auto dfs = [&](auto&& dfs, int u, int opt) {
+  auto dfs = [&](auto&& dfs, int u, int opt, int p) {
     int& ret = dp[u][opt];
     if (ret != IMP) {
       return ret;
@@ -49,16 +35,16 @@ int main() {
     ret = opt ? 1 : 0;
 
     for (auto v : adj[u]) {
-      if (p[u] != v) {
+      if (p != v) {
         if (!opt) {
-          ret += dfs(dfs, v, 1);
+          ret += dfs(dfs, v, 1, u);
         } else {
-          ret += std::min(dfs(dfs, v, 0), dfs(dfs, v, 1));
+          ret += std::min(dfs(dfs, v, 0, u), dfs(dfs, v, 1, u));
         }
       }
     }
     return ret;
   };
 
-  std::println("{}", dfs(dfs, n, 1) - 1);
+  std::println("{}", std::min(dfs(dfs, 0, 1, -1), dfs(dfs, 0, 0, -1)));
 }
